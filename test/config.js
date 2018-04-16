bb10322c08deabc5838dd17cbb277de3c950ebc9
Douglas <douglas.rice@hmcts>
2018-04-16 17:32:04 +0100

    test

--- test/config.js
+++ test/config.js
@@ -1,9 +1,14 @@
 module.exports = {
 
-    TestIdamBaseUrl: process.env.TEST_IDAM_API_URL || 'http://localhost:8484',
-    TestFrontendUrl: process.env.TEST_E2E_FRONTEND_URL || 'https://localhost:3000',
+    // TestIdamBaseUrl: process.env.TEST_IDAM_API_URL || 'http://localhost:8484',
+    // TestFrontendUrl: process.env.TEST_E2E_FRONTEND_URL || 'https://localhost:3000',
+    // TestUseIdam: process.env.TEST_USE_IDAM || 'true',
+    // TestIdamLoginUrl: process.env.TEST_IDAM_LOGIN_URL || 'https://localhost:8000/login',
+
+    TestIdamBaseUrl: process.env.TEST_IDAM_API_URL || 'https://idam-test.dev.ccidam.reform.hmcts.net',
+    TestFrontendUrl: process.env.TEST_E2E_FRONTEND_URL || 'https://www-test.probate.reform.hmcts.net',
     TestUseIdam: process.env.TEST_USE_IDAM || 'true',
-    TestIdamLoginUrl: process.env.TEST_IDAM_LOGIN_URL || 'https://localhost:8000/login',
+    TestIdamLoginUrl: process.env.TEST_IDAM_LOGIN_URL || 'https://idam-test.dev.ccidam.reform.hmcts.net/login',
 
     TestIdamAddUserUrl: '/testing-support/accounts',
     TestIdamRole: 'probate-private-beta',
@@ -19,8 +24,8 @@ module.exports = {
     links: {
         cookies: 'https://www.gov.uk/help/cookies',
         terms: '/terms-conditions',
-        survey: 'http://www.smartsurvey.co.uk/',
-        surveyEndOfApplication: 'http://www.smartsurvey.co.uk/'
+        survey: 'http://www.smartsurvey.co.uk/s/CFZF7/',
+        surveyEndOfApplication: 'http://www.smartsurvey.co.uk/s/A2LY8/'
     },
 
 

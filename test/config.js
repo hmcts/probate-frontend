6be14d14eeb6c287740299e128cb46627537be27
praveenr <praveen.moj@gmail.com>
2018-04-24 23:02:30 +0100

    corrected for cnp

--- test/config.js
+++ test/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     TestIdamBaseUrl: process.env.TEST_IDAM_API_URL || 'http://localhost:8484',
-    TestFrontendUrl: process.env.TEST_E2E_FRONTEND_URL || 'http://localhost:3000',
+    TestFrontendUrl_backup: process.env.TEST_E2E_FRONTEND_URL || 'http://localhost:3000',
+    TestFrontendUrl: process.env.TEST_URL || 'http://localhost:3000',
     TestUseIdam: process.env.TEST_USE_IDAM || 'true',
     TestIdamLoginUrl: process.env.TEST_IDAM_LOGIN_URL || 'https://localhost:8000/login',
     TestIdamAddUserUrl: '/testing-support/accounts',

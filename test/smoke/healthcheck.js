c75376da90cd005c590156dfac7cd89234fb2c78
praveenr <praveen.moj@gmail.com>
2018-04-26 15:11:58 +0100

    chaged backend env. variables to CNP

--- test/smoke/healthcheck.js
+++ test/smoke/healthcheck.js
@@ -4,7 +4,7 @@ const chaiHttp = require('chai-http');
 const request = require('superagent');
 const config = require('../config');
 //const frontendUrl = config.TestFrontendUrl | ;
-const frontendUrl = process.env.TEST_URL  ;
+const frontendUrl = process.env.TEST_URL || 'http://localhost:3000'
 const healthcheckRequest = (url, cb) => {
     return request
         .get(`${url}/health`)

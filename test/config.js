f2cf925c1203f6637b628437afcb8d13030df959
Douglas <douglas.rice@hmcts>
2018-05-08 10:10:12 +0100

    multiple executors

--- test/config.js
+++ test/config.js
@@ -1,30 +1,27 @@
 module.exports = {
 
-    TestIdamBaseUrl: process.env.TEST_IDAM_API_URL || 'http://localhost:8484',
+    TestIdamBaseUrl: process.env.IDAM_API_URL || 'http://localhost:8484',
     TestFrontendUrl: process.env.TEST_URL || 'https://localhost:3000',
-    TestUseIdam: process.env.TEST_USE_IDAM || 'true',
-    TestIdamLoginUrl: process.env.TEST_IDAM_LOGIN_URL || 'https://localhost:8000/login',
+    TestUseIdam: process.env.USE_IDAM || 'true',
+    TestIdamLoginUrl: process.env.IDAM_LOGIN_URL || 'https://localhost:8000/login',
 
-    TestInviteIdListUrl: '/inviteIdList',
-    TestPinUrl: '/pin',
-    TestInvitationUrl: '/executors/invitation',
-    TestIdamAddUserUrl: '/testing-support/accounts',
-    TestIdamRole: 'probate-private-beta',
-    TestCitizenDomain: '@probateTest.com',
+    TestInviteIdListUrl: process.env.INVITE_ID_LIST_URL,
+    TestPinUrl: process.env.PIN_URL,
+    TestInvitationUrl: process.env.INVITATION_URL,
+    TestIdamAddUserUrl: process.env.IDAM_ADD_USER_URL,
+    TestIdamRole: process.env.IDAM_CITIZEN_ROLE,
+    TestCitizenDomain: process.env.CITIZEN_EMAIL_DOMAIN,
 
     TestGovUkConfirmPaymentUrl: 'www.payments.service.gov.uk',
 
-    TestEnvEmailAddress: 'test.probate.inbox@gmail.com',
-    TestEnvMobileNumber: '07952626390',
-
-    paymentEnvironments: ['test'],
-
+    TestEnvEmailAddress: process.TEST_EMAIL_ADDRESS,
+    TestEnvMobileNumber: process.env.TEST_MOBILE_NUMBER,
 
     links: {
         cookies: 'https://www.gov.uk/help/cookies',
-        terms: '/terms-conditions',
-        survey: 'http://www.smartsurvey.co.uk/s/CFZF7/',
-        surveyEndOfApplication: 'http://www.smartsurvey.co.uk/s/A2LY8/'
+        terms: process.env.TERMS_AND_CONDITIONS,
+        survey: process.env.SURVEY,
+        surveyEndOfApplication: process.env.SURVEY_END_OF_APPLICATION
     },
 
 

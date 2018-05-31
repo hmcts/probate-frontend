cc5ec95cfb08d7ad15853dc5f3ea892a2e8e8825
praveenr <praveen.moj@gmail.com>
2018-05-31 19:28:56 +0100

    Ignore https error

--- test/end-to-end/codecept.conf.js
+++ test/end-to-end/codecept.conf.js
@@ -10,6 +10,7 @@ exports.config = {
             'show': false,
             waitForAction: 2000,
             'switches': {
+                'ignoreHTTPSErrors' : true,
                 'ignore-certificate-errors': true
             }
         },

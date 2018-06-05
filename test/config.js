db2544d82f8f75ed2afc8964d85d6b4c71907b5a
Douglas <douglas.rice@hmcts>
2018-06-05 16:24:06 +0100

    cnp

--- test/config.js
+++ test/config.js
@@ -2,7 +2,7 @@ module.exports = {
 
     TestIdamBaseUrl: process.env.IDAM_API_URL || 'http://localhost:8484',
     TestFrontendUrl: process.env.TEST_URL || 'http://localhost:3000',
-    TestUseIdam: process.env.USE_IDAM || 'false',
+    TestUseIdam: process.env.USE_IDAM || 'true',
     TestIdamLoginUrl: process.env.IDAM_LOGIN_URL || 'https://localhost:8000/login',
 
     TestUseGovPay: process.env.USE_GOV_PAY || 'true',

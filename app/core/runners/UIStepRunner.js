6de91939c32d103ebf41bfa65730d05ff21d34e5
shepda <david@x-designs.co.uk>
2018-06-06 16:35:09 +0100

    Remove unused logging

--- app/core/runners/UIStepRunner.js
+++ app/core/runners/UIStepRunner.js
@@ -4,7 +4,6 @@ const co = require('co');
 const {curry, set, isEmpty, forEach} = require('lodash');
 const mapErrorsToFields = require('app/components/error').mapErrorsToFields;
 const DetectDataChange = require('app/wrappers/DetectDataChange');
-const logger = require('app/components/logger');
 
 class UIStepRunner {
 
@@ -15,33 +14,23 @@ class UIStepRunner {
 
     handleGet(step, req, res) {
 
-        logger().info('called handleGet');
-
         return co(function * () {
-            logger().info('called handleGet 2');
             let errors = null;
             const session = req.session;
-            logger().info('got session data');
             const formdata = session.form;
-            logger().info('got form data');
             let ctx = step.getContextData(req);
-            logger().info('called step.getContextData');
             [ctx, errors] = yield step.handleGet(ctx, formdata);
-            logger().info('called step.handleGet');
             forEach(errors, (error) =>
             req.log.info({type: 'Validation Message', url: step.constructor.getUrl()}, JSON.stringify(error))
         );
         const content = step.generateContent(ctx, formdata);
-        logger().info('called step.generateContent');
         const fields = step.generateFields(ctx, errors, formdata);
-        logger().info('called step.generateFields');
         if (req.query.source === 'back') {
             session.back.pop();
         } else if (session.back[session.back.length - 1] !== step.constructor.getUrl()) {
             session.back.push(step.constructor.getUrl());
         }
         const common = step.commonContent();
-        logger().info('called step.commonContent');
             res.render(step.template, {content, fields, errors, common});
         }).catch((error) => {
             req.log.error(error);

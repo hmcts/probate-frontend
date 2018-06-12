ee8e769cf4a5f61f7df767f061abac825a97199c
Douglas <douglas.rice@hmcts>
2018-06-12 15:58:38 +0100

    CNP

--- app/healthcheck.js
+++ app/healthcheck.js
@@ -3,16 +3,16 @@
 const router = require('express').Router();
 const os = require('os');
 const gitProperties = require('git.properties');
-const Healthcheck = require('app/utils/Healthcheck');
+//const Healthcheck = require('app/utils/Healthcheck');
 const commonContent = require('app/resources/en/translation/common');
 const gitRevision = process.env.GIT_REVISION;
 const osHostname = os.hostname();
 const gitCommitId = gitProperties.git.commit.id;
 
 router.get('/', (req, res) => {
-    const healthcheck = new Healthcheck();
-    healthcheck.getDownstream(healthcheck.health, healthDownstream => {
-        healthcheck.getDownstream(healthcheck.info, infoDownstream => {
+    //const healthcheck = new Healthcheck();
+    //healthcheck.getDownstream(healthcheck.health, healthDownstream => {
+       // healthcheck.getDownstream(healthcheck.info, infoDownstream => {
             return res.json({
                 name: commonContent.serviceName,
                 status: 'UP',
@@ -20,10 +20,10 @@ router.get('/', (req, res) => {
                 host: osHostname,
                 version: gitRevision,
                 gitCommitId,
-                downstream: healthcheck.mergeInfoAndHealthData(healthDownstream, infoDownstream)
+         //       downstream: healthcheck.mergeInfoAndHealthData(healthDownstream, infoDownstream)
             });
-        });
-    });
+       // });
+    //});
 });
 
 module.exports = router;

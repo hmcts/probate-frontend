1201a026326bd0419fbac028580a7db571a060c5
Tom Elliott <tomxelliott@hotmail.co.uk>
2018-07-06 12:42:32 +0100

    PRO-1699: Added Idam test for signOut page. Test for CoApplicantStartPage not working

--- test/component/testSignOut.js
+++ test/component/testSignOut.js
@@ -1,19 +1,19 @@
 'use strict';
 const TestWrapper = require('test/util/TestWrapper');
 const commonContent = require('app/resources/en/translation/common');
+const {expect} = require('chai');
 
 describe('sign-out', () => {
     let testWrapper;
 
-    beforeEach(() => {
-        testWrapper = new TestWrapper('SignOut');
-    });
-
-    afterEach(() => {
-        testWrapper.destroy();
-    });
-
     describe('Verify Content, Errors and Redirection', () => {
+        beforeEach(() => {
+            testWrapper = new TestWrapper('SignOut');
+        });
+
+        afterEach(() => {
+            testWrapper.destroy();
+        });
 
         it('test right content loaded on the page', (done) => {
             const excludeKeys = [];
@@ -28,4 +28,24 @@ describe('sign-out', () => {
             testWrapper.testContentNotPresent(done, playbackData);
         });
     });
+
+    it('test page renders without being redirected to idam', (done) => {
+        const request = require('supertest');
+        const server = new (require('app').init)();
+        server.useIDAM = 'true';
+        const agent = request.agent(server.app);
+        agent.get('/sign-out')
+            .expect('Content-type', /html/)
+            .expect(200)
+            .end((err, res) => {
+                server.http.close();
+                if (err) {
+                    done(err);
+                } else {
+                    const text = res.text.toLowerCase();
+                    expect(text).to.contain('you&rsquo;ve signed out');
+                    done();
+                }
+            });
+    }).timeout(5000);
 });

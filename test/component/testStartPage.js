629389084e21d15e73590e0ea161fbf4a2188f83
Tom Elliott <tomxelliott@hotmail.co.uk>
2018-07-06 14:46:21 +0100

    PRO-3065: Add tests to ensure that Start-Page is rendered properly without redirecting to Idam

--- test/component/testStartPage.js
+++ test/component/testStartPage.js
@@ -1,19 +1,19 @@
-const TestWrapper = require('test/util/TestWrapper'),
-    TaskList = require('app/steps/ui/tasklist/index');
+const TestWrapper = require('test/util/TestWrapper');
+const TaskList = require('app/steps/ui/tasklist/index');
+const {expect} = require('chai');
 
 describe('start-page', () => {
     let testWrapper;
     const expectedNextUrlForTaskList = TaskList.getUrl();
 
-    beforeEach(() => {
-        testWrapper = new TestWrapper('StartPage');
-    });
-
-    afterEach(() => {
-        testWrapper.destroy();
-    });
-
     describe('Verify Content, Errors and Redirection', () => {
+        beforeEach(() => {
+            testWrapper = new TestWrapper('StartPage');
+        });
+
+        afterEach(() => {
+            testWrapper.destroy();
+        });
 
         it('test right content loaded on the page', (done) => {
             const excludeKeys = [];
@@ -26,4 +26,23 @@ describe('start-page', () => {
         });
 
     });
+
+    it('test page renders without being redirected to idam', (done) => {
+        const request = require('supertest');
+        const server = new (require('app').init)();
+        const agent = request.agent(server.app);
+        agent.get('/start-page')
+            .expect('Content-type', /html/)
+            .expect(200)
+            .end((err, res) => {
+                server.http.close();
+                if (err) {
+                    done(err);
+                } else {
+                    const text = res.text.toLowerCase();
+                    expect(text).to.contain('use this service to apply for probate');
+                    done();
+                }
+            });
+    }).timeout(5000);
 });

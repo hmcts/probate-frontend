1ff3eac8aefa7898b4acf186f0bf832059d4833a
vakepati <venkata.akepati@hmcts.net>
2018-08-23 10:34:01 +0100

    added crossbrowser configuration for nightlypipeline

--- test/end-to-end/helpers/SauceLabsReportingHelper.js
+++ test/end-to-end/helpers/SauceLabsReportingHelper.js
@@ -5,14 +5,14 @@ const container = require('codeceptjs').container;
 const exec = require('child_process').exec;
 
 function updateSauceLabsResult(result, sessionId) {
-    return 'curl -X PUT -s -d \'{"passed": ' + result + '}\' -u ' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + ' https://saucelabs.com/rest/v1/' + process.env.SAUCE_USERNAME + '/jobs/' + sessionId;
+console.log('SauceOnDemandSessionID=' + sessionId + 'job-name=ProbateFrontend' ); // eslint-disable-line no-console
+  return 'curl -X PUT -s -d \'{"passed": ' + result + '}\' -u ' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + ' https://saucelabs.com/rest/v1/' + process.env.SAUCE_USERNAME + '/jobs/' + sessionId;
 }
 
 module.exports = function() {
 
     // Setting test success on SauceLabs
     event.dispatcher.on(event.test.passed, () => {
-
         const sessionId = container.helpers('WebDriverIO').browser.requestHandler.sessionID;
         exec(updateSauceLabsResult('true', sessionId));
 
@@ -28,6 +28,7 @@ module.exports = function() {
 
     event.dispatcher.on(event.all.result, () => {
         const sessionId = container.helpers('WebDriverIO').browser.requestHandler.sessionID;
+         exec(updateSauceLabsResult('true', sessionId));
         console.log('SauceOnDemandSessionID=' + sessionId + ' job-name=ProbateFrontend'); // eslint-disable-line no-console
       });
 };

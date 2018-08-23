c472a38a252f2659cfd8b6e4270a62d62272d9f1
vakepati <venkata.akepati@hmcts.net>
2018-08-23 11:07:46 +0100

    added crossbrowser configuration for nightlypipeline

--- test/end-to-end/helpers/SauceLabsReportingHelper.js
+++ test/end-to-end/helpers/SauceLabsReportingHelper.js
@@ -5,8 +5,8 @@ const container = require('codeceptjs').container;
 const exec = require('child_process').exec;
 
 function updateSauceLabsResult(result, sessionId) {
-console.log('SauceOnDemandSessionID=' + sessionId + 'job-name=ProbateFrontend' ); // eslint-disable-line no-console
-  return 'curl -X PUT -s -d \'{"passed": ' + result + '}\' -u ' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + ' https://saucelabs.com/rest/v1/' + process.env.SAUCE_USERNAME + '/jobs/' + sessionId;
+    console.log('SauceOnDemandSessionID=' + sessionId + 'job-name=PipelineTestRepo' ); // eslint-disable-line no-console
+    return 'curl -X PUT -s -d \'{"passed": ' + result + '}\' -u ' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + ' https://saucelabs.com/rest/v1/' + process.env.SAUCE_USERNAME + '/jobs/' + sessionId;
 }
 
 module.exports = function() {
@@ -28,7 +28,6 @@ module.exports = function() {
 
     event.dispatcher.on(event.all.result, () => {
         const sessionId = container.helpers('WebDriverIO').browser.requestHandler.sessionID;
-         exec(updateSauceLabsResult('true', sessionId));
         console.log('SauceOnDemandSessionID=' + sessionId + ' job-name=ProbateFrontend'); // eslint-disable-line no-console
       });
 };

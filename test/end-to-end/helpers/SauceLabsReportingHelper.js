b8ddd9c8d30367d72496dc7bca6493524459123d
vakepati <venkata.akepati@hmcts.net>
2018-08-24 16:51:34 +0100

    added crossbrowser configuration for nightlypipeline

--- test/end-to-end/helpers/SauceLabsReportingHelper.js
+++ test/end-to-end/helpers/SauceLabsReportingHelper.js
@@ -3,7 +3,7 @@ const container = require('codeceptjs').container;
 const exec = require('child_process').exec;
 
 function updateSauceLabsResult(result, sessionId) {
-    console.log('SauceOnDemandSessionID=' + sessionId + 'job-name=PipelineTestRepo'); // eslint-disable-line no-console
+    console.log('SauceOnDemandSessionID=' + sessionId + 'job-name=ProbateFrontend'); // eslint-disable-line no-console
     return 'curl -X PUT -s -d \'{"passed": ' + result + '}\' -u ' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + ' https://saucelabs.com/rest/v1/' + process.env.SAUCE_USERNAME + '/jobs/' + sessionId;
 }
 

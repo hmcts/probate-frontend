42aee365f6dee95ccb3858e6c706a98d17496504
Tom Elliott <tomxelliott@hotmail.co.uk>
2018-09-13 16:47:18 +0100

    PRO-3360: Amend testWrapper function for substitute content and write new tests to ensure correct content for executors is rendered

--- test/unit/testApplicantAliasReason.js
+++ test/unit/testApplicantAliasReason.js
@@ -63,7 +63,7 @@ describe('ApplicantAliasReason', () => {
             done();
         });
 
-        it('should not remove otherReason from the ctx when the aliasReason is not set to other', (done) => {
+        it('should not remove otherReason from the ctx when the aliasReason is set to other', (done) => {
             ctx = {
                 aliasReason: 'other',
                 otherReason: 'because I wanted to'

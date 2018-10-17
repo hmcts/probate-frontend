5013a636cf45b3a711a54bb762374cdda2eda6d1
Luca Del Buono <LucaDelBuono@users.noreply.github.com>
2018-10-17 13:02:00 +0100

    PRO-3082: Exclude screening questions from accessibility test

--- test/accessibility/a11yTest.js
+++ test/accessibility/a11yTest.js
@@ -10,7 +10,7 @@ const {endsWith} = require('lodash');
 const sinon = require('sinon');
 const commonContent = require('app/resources/en/translation/common');
 const services = require('app/components/services');
-const stepsToExclude = ['StartEligibility', 'StartApply', 'NewStartEligibility', 'NewStartApply', 'PinPage', 'PinSent', 'PinResend', 'AddressLookup', 'ExecutorAddress', 'ExecutorContactDetails', 'ExecutorName', 'ExecutorNotified', 'ExecutorNameAsOnWill', 'ExecutorApplying', 'DeleteExecutor', 'PaymentStatus', 'AddAlias', 'RemoveAlias', 'ExecutorRoles', 'ExecutorsWhenDied'];
+const stepsToExclude = ['StartEligibility', 'WillLeft', 'WillOriginal', 'DeathCertificate', 'DeceasedDomicile', 'ApplicantExecutor', 'MentalCapacity', 'IhtCompleted', 'StartApply', 'PinPage', 'PinSent', 'PinResend', 'AddressLookup', 'ExecutorAddress', 'ExecutorContactDetails', 'ExecutorName', 'ExecutorNotified', 'ExecutorNameAsOnWill', 'ExecutorApplying', 'DeleteExecutor', 'PaymentStatus', 'AddAlias', 'RemoveAlias', 'ExecutorRoles', 'ExecutorsWhenDied'];
 const steps = initSteps.steps;
 let checkAllAgreedStub;
 let featureToggleStub;

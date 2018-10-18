8f5390637d7756f8ff45aef4309b5de0f18c1c83
Jake <jake.huggins@solirius.com>
2018-10-18 10:00:43 +0100

    PRO-3667: add config file for steps

---
+++ test/mutation/mutation_steps.conf.js
@@ -0,0 +1,41 @@
+const strykerConfiguration = config => {
+  config.set({
+    testRunner: 'mocha',
+    mutator: 'javascript',
+    transpilers: [],
+    reporter:
+      [
+        'clear-text',
+        'progress',
+        'html'
+      ],
+    testFramework: 'mocha',
+    coverageAnalysis: 'perTest',
+    mutate:
+      [
+        'app/steps/ui/**/index.js',
+        '!test/component/executors/*.js'
+      ],
+    files: ['*.js', '*.json', 'app/**', 'test/**'],
+    maxConcurrentTestRunners: 1,
+    symlinkNodeModules: true,
+    htmlReporter: {baseDir: 'functional-output/mutation-steps'},
+    mochaOptions: {
+      files:
+        [
+        'test/unit/*'
+        ],
+      timeout: 8000
+    },
+    logLevel: 'debug',
+    plugins:
+      [
+        'stryker-mocha-runner',
+        'stryker-mocha-framework',
+        'stryker-javascript-mutator',
+        'stryker-html-reporter'
+      ]
+  });
+};
+
+module.exports = strykerConfiguration;

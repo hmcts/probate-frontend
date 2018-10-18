0a4737b958fa2ed0cc70f069408de520791002d1
shepda <david@x-designs.co.uk>
2018-10-18 16:30:39 +0100

    PRO-3772: Add document type validation

--- app/utils/DocumentUpload.js
+++ app/utils/DocumentUpload.js
@@ -1,25 +1,44 @@
 'use strict';
 
+const fileType = require('file-type');
+const config = require('app/config');
+
 class DocumentUpload {
-    static initDocuments(formdata) {
+    initDocuments(formdata) {
         if (!formdata.documents) {
             formdata.documents = {};
         }
         return formdata;
     }
 
-    static addDocument(uploadedFile, uploads = []) {
-        if (uploadedFile) {
-            uploads.push(uploadedFile);
+    addDocument(uploadedDocument, uploads = []) {
+        if (uploadedDocument.originalname) {
+            uploads.push({
+                filename: uploadedDocument.originalname
+            });
         }
         return uploads;
     }
 
-    static removeDocument(index, uploads) {
-        if (!isNaN(index)) {
+    removeDocument(index, uploads = []) {
+        if (!isNaN(index) && uploads.length > 0) {
             uploads.splice(index, 1);
         }
         return uploads;
+    }
+
+    isValidType(document) {
+        const uploadedDocumentType = fileType(document.buffer);
+        if (!uploadedDocumentType) {
+            return false;
+        }
+        return config.documentUpload.validTypes.includes(uploadedDocumentType.ext);
+    }
+
+    isDocumentValid(document) {
+        let isValid = true;
+        isValid = this.isValidType(document);
+        return isValid;
     }
 }
 

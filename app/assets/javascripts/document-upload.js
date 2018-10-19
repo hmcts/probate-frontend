c3dc3d6b1498c54f743fddf9888969e983752097
shepda <david@x-designs.co.uk>
2018-10-19 15:59:08 +0100

    PRO-3773: Rename variables and change max upload size from mb to bytes

--- app/assets/javascripts/document-upload.js
+++ app/assets/javascripts/document-upload.js
@@ -20,7 +20,7 @@ var DocumentUpload = {
             dictRemoveFile: documentUploadConfig.content.removeFileText,
             dictInvalidFileType: documentUploadConfig.content.invalidFileType,
             dictMaxFilesExceeded: documentUploadConfig.content.maxFilesExceeded,
-            dictFileTooBig: documentUploadConfig.content.maxFileSize
+            dictFileTooBig: documentUploadConfig.content.maxSize
         })
         .on('addedfile', function() {
             DocumentUpload.hideEmptyListMessage();

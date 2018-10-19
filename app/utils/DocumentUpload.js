496c17c12d662ea45eb0cfe957d50301ee81535e
shepda <david@x-designs.co.uk>
2018-10-19 16:00:41 +0100

    PRO-3773: Add file size validation and change method to return error messages

--- app/utils/DocumentUpload.js
+++ app/utils/DocumentUpload.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const fileType = require('file-type');
-const config = require('app/config');
+const config = require('app/config').documentUpload;
+const content = require('app/resources/en/translation/common');
 
 class DocumentUpload {
     initDocuments(formdata) {
@@ -43,10 +44,28 @@ class DocumentUpload {
         return validMimeTypes.includes(uploadedDocumentType.mime);
     }
 
-    isDocumentValid(document) {
-        let isValid = true;
-        isValid = this.isValidType(document);
-        return isValid;
+    isValidSize(document) {
+        return document.size <= config.maxSize;
+    }
+
+    error(document) {
+        let error = null;
+
+        if (error === null && !this.isValidType(document)) {
+            error = {
+                js: content.documentUploadInvalidFileType,
+                nonJs: 'type'
+            };
+        }
+
+        if (error === null && !this.isValidSize(document)) {
+            error = {
+                js: content.documentUploadMaxSize,
+                nonJs: 'maxSize'
+            };
+        }
+
+        return error;
     }
 }
 

e4d54599691aaf3ce3f9b55a21c73210a89044a2
Jake <jake.huggins@solirius.com>
2018-10-22 20:15:19 +0100

    PRO-3667: update mutation unittest conf
    configuration file was pulling in too many files and causing it to run out of memory when performing mutation tests

--- test/mutation/mutation_unittests.conf.js
+++ test/mutation/mutation_unittests.conf.js
@@ -1,47 +1,47 @@
 const strykerConfiguration = config => {
-  config.set({
-    testRunner: 'mocha',
-    mutator: 'javascript',
-    transpilers: [],
-    reporter:
+    config.set({
+        testRunner: 'mocha',
+        mutator: 'javascript',
+        transpilers: [],
+        reporter:
       [
-        'clear-text',
-        'progress',
-        'html'
+          'clear-text',
+          'progress',
+          'html'
       ],
-    testFramework: 'mocha',
-    coverageAnalysis: 'perTest',
-    mutate:
+        testFramework: 'mocha',
+        coverageAnalysis: 'perTest',
+        mutate:
       [
-        'app/steps/ui/**/index.js',
-        'app/*.js',
-        'app/core/**',
-        'app/wrappers/*.js',
-        'app/journeys/*',
-        'app/utils/*',
-        '!test/component/executors/*.js',
-        '!app/config.js',
+          'app/steps/ui/**/index.js',
+          // 'app/*.js',
+          'app/core/**',
+          'app/wrappers/*.js',
+          // 'app/journeys/*',
+          // 'app/utils/*',
+          // '!test/**',
+          // '!app/config.js',
       ],
-    files: ['*.js', '*.json', 'app/**', 'test/**'],
-    maxConcurrentTestRunners: 1,
-    symlinkNodeModules: true,
-    htmlReporter: {baseDir: 'functional-output/mutation-unittests'},
-    mochaOptions: {
-      files:
+        files: ['*.js', '*.json', 'app/**', 'test/unit/*', 'test/data/**', 'test/service-stubs/**'],
+        maxConcurrentTestRunners: 1,
+        symlinkNodeModules: true,
+        htmlReporter: {baseDir: 'functional-output/mutation-unittests'},
+        mochaOptions: {
+            files:
         [
-        'test/unit/*'
+            'test/unit/*'
         ],
-      timeout: 8000
-    },
-    logLevel: 'debug',
-    plugins:
+            timeout: 8000
+        },
+        // logLevel: 'debug',
+        plugins:
       [
-        'stryker-mocha-runner',
-        'stryker-mocha-framework',
-        'stryker-javascript-mutator',
-        'stryker-html-reporter'
+          'stryker-mocha-runner',
+          'stryker-mocha-framework',
+          'stryker-javascript-mutator',
+          'stryker-html-reporter'
       ]
-  });
+    });
 };
 
 module.exports = strykerConfiguration;

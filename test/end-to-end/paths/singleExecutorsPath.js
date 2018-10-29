170f4e17a16f328a5ff46d02d6b02f99a645b8ae
Muhammad Husain <muhammad.husain@hmcts.net>
2018-10-29 15:14:50 +0000

    PRO-4002: fixed eslint issue on preview

--- test/end-to-end/paths/singleExecutorsPath.js
+++ test/end-to-end/paths/singleExecutorsPath.js
@@ -11,15 +11,11 @@ Feature('Single Executor flow');
 // eslint complains that the Before/After are not used but they are by codeceptjs
 // so we have to tell eslint to not validate these
 // eslint-disable-next-line no-undef
-Before(async () => {
-    try {
-        await TestConfigurator.getBefore();
-        //        isAliasToggledEnabled = await services.featureToggle(config.featureToggles.main_applicant_alias);
-        // console.log('isAliasToggledEnabled =', isAliasToggledEnabled);
-        // console.log('isAliasToggledEnabled type =', typeof isAliasToggledEnabled);
-    } catch (err) {
-        throw new Error(err);
-    }
+Before(() => {
+    TestConfigurator.getBefore();
+    //        isAliasToggledEnabled = await services.featureToggle(config.featureToggles.main_applicant_alias);
+    // console.log('isAliasToggledEnabled =', isAliasToggledEnabled);
+    // console.log('isAliasToggledEnabled type =', typeof isAliasToggledEnabled);
 });
 // eslint-disable-next-line no-undef
 After(() => {

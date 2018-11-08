dcd217715f6d7835a51083526de01c863b7102ec
Tom Elliott <tomxelliott@hotmail.co.uk>
2018-11-08 11:28:57 +0000

    PRO-3966: Fix issues after merge with master

--- test/component/will/testWillCodicils.js
+++ test/component/will/testWillCodicils.js
@@ -49,7 +49,7 @@ describe('will-codicils', () => {
             const data = {
                 codicils: 'No'
             };
-            testWrapper.testRedirect(done, data, expectedNextUrlForDeathCertificate);
+            testWrapper.testRedirect(done, data, expectedNextUrlForTaskList);
         });
 
         it(`test it redirects to tasklist page: ${expectedNextUrlForTaskList}`, (done) => {

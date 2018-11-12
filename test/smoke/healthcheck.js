a1a34d1b814e6a0b4e5c3e126f271c9e3c487a8b
Doug Rice <Douglas.Rice@hmcts.net>
2018-11-12 17:04:03 +0000

    test

--- test/smoke/healthcheck.js
+++ test/smoke/healthcheck.js
@@ -1,12 +1,14 @@
 const chai = require('chai');
-const expect = require('chai').expect;
+//const expect = require('chai').expect;
 const chaiHttp = require('chai-http');
-const request = require('superagent');
-const config = require('../config');
-const frontendUrl = config.TestFrontendUrl;
+//const request = require('superagent');
+//const config = require('../config');
+//const frontendUrl = config.TestFrontendUrl;
 //const frontendUrl = (process.env.TEST_URL || 'https://localhost:3000')
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+/*
 const healthcheckRequest = (url, cb) => {
     return request
         .get(`${url}/health`)
@@ -17,6 +19,7 @@ const healthcheckRequest = (url, cb) => {
             cb(res);
         });
 };
+*/
 
 chai.use(chaiHttp);
 

395d0113484e50223230f301a9bc37e7b4d46b61
Luca Del Buono <LucaDelBuono@users.noreply.github.com>
2018-11-22 10:58:04 +0000

    PRO-3484: Amends to the Documents page

--- app/featureToggles.js
+++ app/featureToggles.js
@@ -54,5 +54,6 @@ router.get('/executor-current-name-reason/:index', (req, res, next) => featureTo
 
 router.get('/document-upload', (req, res, next) => featureToggle.callCheckToggle(req, res, next, 'document_upload', featureToggle.togglePage, '/tasklist'));
 router.get('/deceased-address', (req, res, next) => featureToggle.callCheckToggle(req, res, next, 'document_upload', featureToggle.toggleFeature));
+router.get('/documents', (req, res, next) => featureToggle.callCheckToggle(req, res, next, 'document_upload', featureToggle.toggleFeature));
 
 module.exports = router;

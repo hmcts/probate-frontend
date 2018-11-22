6cd11a1f33180eea71067fa662c725944240d331
Muhammad Husain <muhammad.husain@hmcts.net>
2018-11-22 10:47:48 +0000

    PRO-3966: removed toggles for Applicant alias

--- test/config.js
+++ test/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     TestIdamBaseUrl: process.env.IDAM_API_URL || 'http://localhost:8484',
-    TestFrontendUrl: process.env.TEST_URL || 'http://localhost:3000',
+    TestFrontendUrl: 'https://probate-frontend-aat.service.core-compute-aat.internal', //.env.TEST_URL || 'http://localhost:3000',
     TestE2EFrontendUrl: process.env.TEST_E2E_URL || 'http://localhost:3000',
     TestUseIdam: process.env.USE_IDAM || 'false',
     TestUseSidam: process.env.USE_SIDAM || 'true',

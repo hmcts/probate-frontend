a5b7fafe889226f017da8122449ae8e4ffb3dcbe
Doug Rice <Douglas.Rice@hmcts.net>
2019-01-17 14:06:40 +0000

    PRO-1234: Test

--- test/end-to-end/pages/documents/documents.js
+++ test/end-to-end/pages/documents/documents.js
@@ -2,11 +2,12 @@
 
 const pageUnderTest = require('app/steps/ui/documents/index');
 const commonContent = require('app/resources/en/translation/common');
+const documents = require('app/resources/en/translation/documents');
 
 module.exports = function () {
     const I = this;
     I.seeCurrentUrlEquals(pageUnderTest.getUrl());
 
-    I.click('#coverSheetPdfHref');
+    I.click(documents.coverSheetPdf);
     I.click(commonContent.continue);
 };

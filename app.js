85b2a953140991cdc29c7114c3bc6db90787705d
Luca Del Buono <luca.delbuono@gmail.com>
2019-04-23 15:32:48 +0100

    Add space

--- app.js
+++ app.js
@@ -66,6 +66,7 @@ exports.init = function() {
     app.set('views', ['app/steps', 'app/views', 'node_modules/govuk_template_jinja/views/layouts']);
 
     const filters = require('app/components/filters.js');
+
     const globals = {
         'currentYear': new Date().getFullYear(),
         'gaTrackingId': config.gaTrackingId,

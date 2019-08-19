c451dc51093bdb77d7b938c1b4abe61b265f9e3e
Luca Del Buono <luca.delbuono@gmail.com>
2019-08-19 13:29:29 +0100

    Temporarily hardcoded mock applications in getContextData

--- app/steps/ui/dashboard/index.js
+++ app/steps/ui/dashboard/index.js
@@ -1,11 +1,28 @@
 'use strict';
 
 const Step = require('app/core/steps/Step');
+const content = require('app/resources/en/translation/dashboard');
 
 class Dashboard extends Step {
 
     static getUrl() {
         return '/dashboard';
+    }
+
+    getContextData(req) {
+        const ctx = super.getContextData(req);
+
+        ctx.applications = [{
+            deceasedFullName: 'Bob Jones',
+            dateCreated: '7 October 2018',
+            status: content.statusInProgress
+        }, {
+            deceasedFullName: 'Tom Smith',
+            dateCreated: '24 February 2019',
+            status: content.statusSubmitted
+        }];
+
+        return ctx;
     }
 }
 

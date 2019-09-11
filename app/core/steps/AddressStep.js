0e0d2a7b39e3ff0bacd34bd90c27ae3f59a73d21
Luca Del Buono <luca.delbuono@gmail.com>
2019-09-11 12:36:53 +0100

    PRO-6099: Better fix for FE bug (better for accessibility)

--- app/core/steps/AddressStep.js
+++ app/core/steps/AddressStep.js
@@ -38,6 +38,7 @@ class AddressStep extends ValidationStep {
             county: ctx.county,
             country: ctx.country
         };
+        ctx.postcode = ctx.postcode ? ctx.postcode.toUpperCase() : ctx.postcode;
         ctx.address.formattedAddress = this.getFormattedAddress(ctx.address);
 
         return [ctx, errors];

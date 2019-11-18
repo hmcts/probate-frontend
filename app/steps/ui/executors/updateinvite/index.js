f571d5799c5a2124d49ac35fbb581a7bd36ca3bf
Megan <meganaliceslattery@gmail.com>
2019-11-18 13:33:03 +0000

    PRO-6509: Move update call to handle post on update-invite page

--- app/steps/ui/executors/updateinvite/index.js
+++ app/steps/ui/executors/updateinvite/index.js
@@ -2,6 +2,11 @@
 
 const ValidationStep = require('app/core/steps/ValidationStep');
 const ExecutorsWrapper = require('app/wrappers/Executors');
+const FormatName = require('app/utils/FormatName');
+const logger = require('app/components/logger')('Init');
+const InviteLink = require('app/services/InviteLink');
+const config = require('app/config');
+
 
 class ExecutorsUpdateInvite extends ValidationStep {
 
@@ -14,8 +19,57 @@ class ExecutorsUpdateInvite extends ValidationStep {
         const formdata = req.session.form;
         const executorsWrapper = new ExecutorsWrapper(formdata.executors);
         ctx.executorsEmailChangedList = executorsWrapper.executorsEmailChangedList();
-        ctx.inviteSuffix = ctx.executorsEmailChangedList.length > 1 ? '-multiple' : '';
+        ctx.inviteSuffix = ctx.executorsNumber > 2 ? '-multiple' : '';
+        ctx.authToken = req.authToken;
+        ctx.serviceAuthorization = req.session.serviceAuthorization;
         return ctx;
+    }
+
+    * handlePost(ctx, errors, formdata) {
+        const inviteLink = new InviteLink(config.services.orchestrator.url, ctx.sessionID);
+        const executorsWrapper = new ExecutorsWrapper(formdata.executors);
+        formdata.executors.list = executorsWrapper.addExecutorIds();
+        let executorsToNotifyList = executorsWrapper.executorsEmailChangedList();
+
+        executorsToNotifyList = executorsToNotifyList
+            .map(exec => {
+                return {
+                    id: exec.id,
+                    executorName: exec.fullName,
+                    firstName: formdata.deceased.firstName,
+                    lastName: formdata.deceased.lastName,
+                    email: exec.email,
+                    phoneNumber: exec.mobile,
+                    formdataId: formdata.ccdCase.id,
+                    leadExecutorName: FormatName.format(formdata.applicant)
+                };
+            });
+
+        if (executorsToNotifyList.length) {
+            yield inviteLink.post(executorsToNotifyList, ctx.authToken, ctx.serviceAuthorization)
+                .then(result => {
+                    if (result.name === 'Error') {
+                        logger.error(`Error while sending executor email invites: ${result}`);
+                        throw new ReferenceError('Error while sending co-applicant invitation emails.');
+                    } else {
+                        result.invitations.forEach((execResult) => {
+                            const result = {
+                                inviteId: execResult.inviteId
+                            };
+                            Object.assign(formdata.executors.list.find(execList => execList.email === execResult.email && execList.fullName === execResult.executorName), result);
+                        });
+                        formdata.executors.list = executorsWrapper.removeExecutorIds();
+                        formdata.executors.list = executorsWrapper.removeExecutorsEmailChangedFlag();
+                    }
+                });
+        }
+
+        yield new Promise((resolve) => resolve())
+            .then(() => {
+                formdata.executors.list = executorsWrapper.removeExecutorIds();
+            });
+
+        return [ctx, errors];
     }
 
     action(ctx, formdata) {

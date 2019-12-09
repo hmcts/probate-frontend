4044bab11864627b969ae628e94488a71510495e
Megan <meganaliceslattery@gmail.com>
2019-12-09 15:27:12 +0000

    PRO-6509: delete exec id

--- app/steps/ui/executors/updateinvite/index.js
+++ app/steps/ui/executors/updateinvite/index.js
@@ -33,7 +33,6 @@ class ExecutorsUpdateInvite extends ValidationStep {
         executorsToNotifyList = executorsToNotifyList
             .map(exec => {
                 return {
-                    id: exec.id,
                     executorName: exec.fullName,
                     firstName: formdata.deceased.firstName,
                     lastName: formdata.deceased.lastName,

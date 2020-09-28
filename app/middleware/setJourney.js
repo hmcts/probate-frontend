2760c12bdedae9abcb2c98673d702af88d15fa3a
Megan <meganaliceslattery@gmail.com>
2020-09-28 12:56:07 +0100

    Add death cert journey toggle

--- app/middleware/setJourney.js
+++ app/middleware/setJourney.js
@@ -2,10 +2,17 @@
 
 const probateJourney = require('app/journeys/probate');
 const intestacyJourney = require('app/journeys/intestacy');
+const probateNewDeathCertFlow = require('app/journeys/probatenewdeathcertflow');
 const caseTypes = require('app/utils/CaseTypes');
+const featureToggle = require('app/utils/FeatureToggle');
 
 const setJourney = (req, res, next) => {
     req.session.journey = caseTypes.isIntestacyCaseType(req.session) ? intestacyJourney : probateJourney;
+
+    if (featureToggle.isEnabled(req.session.featureToggles, 'ft_new_deathcert_flow') && !caseTypes.isIntestacyCaseType(req.session)) {
+        req.session.journey = probateNewDeathCertFlow;
+    }
+
     next();
 };
 

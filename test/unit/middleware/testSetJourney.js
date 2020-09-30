f35a670187a314ddf8d1fbe299e1a28adf49904d
Megan <meganaliceslattery@gmail.com>
2020-09-30 12:39:00 +0100

    Add tests for journey changes when feature toggle is on

--- test/unit/middleware/testSetJourney.js
+++ test/unit/middleware/testSetJourney.js
@@ -87,6 +87,108 @@ describe('setJourney', () => {
             revert();
             done();
         });
+
+        it('should set req.journey with the new probate death certificate journey when feature toggle is on and caseType is probate', (done) => {
+            const revert = setJourney.__set__('probateNewDeathCertFlow', {journey: 'a new probate journey'});
+            const req = {
+                session: {
+                    form: {
+                        caseType: caseTypes.GOP
+                    },
+                    featureToggles: {
+                        'ft_new_deathcert_flow': true
+                    }
+                }
+            };
+            const res = {};
+            const next = sinon.spy();
+
+            setJourney(req, res, next);
+
+            expect(req.session).to.deep.equal({
+                form: {
+                    caseType: caseTypes.GOP,
+                },
+                featureToggles: {
+                    'ft_new_deathcert_flow': true
+                },
+                journey: {
+                    journey: 'a new probate journey'
+                }
+            });
+            expect(next.calledOnce).to.equal(true);
+
+            revert();
+            done();
+        });
+
+        it('should set req.journey with the probate journey if feature toggle is off and caseType is probate', (done) => {
+            const revert = setJourney.__set__('probateJourney', {journey: 'a probate journey'});
+            const req = {
+                session: {
+                    form: {
+                        caseType: caseTypes.GOP
+                    },
+                    featureToggles: {
+                        'ft_new_deathcert_flow': false
+                    }
+                }
+            };
+            const res = {};
+            const next = sinon.spy();
+
+            setJourney(req, res, next);
+
+            expect(req.session).to.deep.equal({
+                form: {
+                    caseType: caseTypes.GOP,
+                },
+                featureToggles: {
+                    'ft_new_deathcert_flow': false
+                },
+                journey: {
+                    journey: 'a probate journey'
+                }
+            });
+            expect(next.calledOnce).to.equal(true);
+
+            revert();
+            done();
+        });
+
+        it('should set req.journey with the intestacy journey if feature toggle is on and caseType is intestacy', (done) => {
+            const revert = setJourney.__set__('intestacyJourney', {journey: 'an intestacy journey'});
+            const req = {
+                session: {
+                    form: {
+                        caseType: caseTypes.INTESTACY
+                    },
+                    featureToggles: {
+                        'ft_new_deathcert_flow': false
+                    }
+                }
+            };
+            const res = {};
+            const next = sinon.spy();
+
+            setJourney(req, res, next);
+
+            expect(req.session).to.deep.equal({
+                form: {
+                    caseType: caseTypes.INTESTACY,
+                },
+                featureToggles: {
+                    'ft_new_deathcert_flow': false
+                },
+                journey: {
+                    journey: 'an intestacy journey'
+                }
+            });
+            expect(next.calledOnce).to.equal(true);
+
+            revert();
+            done();
+        });
     });
 
 });

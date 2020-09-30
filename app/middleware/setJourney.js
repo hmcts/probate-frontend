bec25bdf979b01c89621993929e4523ae3514023
Megan <meganaliceslattery@gmail.com>
2020-09-30 16:28:02 +0100

    Add flow for new intestacy journey

--- app/middleware/setJourney.js
+++ app/middleware/setJourney.js
@@ -3,14 +3,15 @@
 const probateJourney = require('app/journeys/probate');
 const intestacyJourney = require('app/journeys/intestacy');
 const probateNewDeathCertFlow = require('app/journeys/probatenewdeathcertflow');
+const intestacyNewDeathCertFlow = require('app/journeys/intestacynewdeathcertflow');
 const caseTypes = require('app/utils/CaseTypes');
 const featureToggle = require('app/utils/FeatureToggle');
 
 const setJourney = (req, res, next) => {
     req.session.journey = caseTypes.isIntestacyCaseType(req.session) ? intestacyJourney : probateJourney;
 
-    if (featureToggle.isEnabled(req.session.featureToggles, 'ft_new_deathcert_flow') && !caseTypes.isIntestacyCaseType(req.session)) {
-        req.session.journey = probateNewDeathCertFlow;
+    if (featureToggle.isEnabled(req.session.featureToggles, 'ft_new_deathcert_flow')) {
+        req.session.journey = caseTypes.isIntestacyCaseType(req.session) ? intestacyNewDeathCertFlow : probateNewDeathCertFlow;
     }
 
     next();
--- test/unit/middleware/testSetJourney.js
+++ test/unit/middleware/testSetJourney.js
@@ -122,49 +122,15 @@ describe('setJourney', () => {
             done();
         });
 
-        it('should set req.journey with the probate journey if feature toggle is off and caseType is probate', (done) => {
-            const revert = setJourney.__set__('probateJourney', {journey: 'a probate journey'});
-            const req = {
-                session: {
-                    form: {
-                        caseType: caseTypes.GOP
-                    },
-                    featureToggles: {
-                        'ft_new_deathcert_flow': false
-                    }
-                }
-            };
-            const res = {};
-            const next = sinon.spy();
-
-            setJourney(req, res, next);
-
-            expect(req.session).to.deep.equal({
-                form: {
-                    caseType: caseTypes.GOP,
-                },
-                featureToggles: {
-                    'ft_new_deathcert_flow': false
-                },
-                journey: {
-                    journey: 'a probate journey'
-                }
-            });
-            expect(next.calledOnce).to.equal(true);
-
-            revert();
-            done();
-        });
-
-        it('should set req.journey with the intestacy journey if feature toggle is on and caseType is intestacy', (done) => {
-            const revert = setJourney.__set__('intestacyJourney', {journey: 'an intestacy journey'});
+        it('should set req.journey with the new intestacy death certificate journey if feature toggle is on and caseType is intestacy', (done) => {
+            const revert = setJourney.__set__('intestacyNewDeathCertFlow', {journey: 'a new intestacy journey'});
             const req = {
                 session: {
                     form: {
                         caseType: caseTypes.INTESTACY
                     },
                     featureToggles: {
-                        'ft_new_deathcert_flow': false
+                        'ft_new_deathcert_flow': true
                     }
                 }
             };
@@ -178,10 +144,10 @@ describe('setJourney', () => {
                     caseType: caseTypes.INTESTACY,
                 },
                 featureToggles: {
-                    'ft_new_deathcert_flow': false
+                    'ft_new_deathcert_flow': true
                 },
                 journey: {
-                    journey: 'an intestacy journey'
+                    journey: 'a new intestacy journey'
                 }
             });
             expect(next.calledOnce).to.equal(true);

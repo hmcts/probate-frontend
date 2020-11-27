19700a62fb654d918c8192205c3076e6cb9518da
Vincent Holt <vleholt@yahoo.co.uk>
2020-11-27 11:38:11 +0000

    output current url on equality page

--- test/end-to-end/pages/equalityanddiversity/startpage.js
+++ test/end-to-end/pages/equalityanddiversity/startpage.js
@@ -5,7 +5,10 @@ const config = require('config');
 module.exports = async function() {
     const I = this;
 
-    await I.checkPageUrl('app/steps/ui/equality');
+    const url = await I.grabCurrentUrl();
+    console.info(`equality url: ${url}`);
+
+    // await I.checkPageUrl('app/steps/ui/equality');
     const backButtonLocator = {css: '#back-button'};
     await I.waitForElement(backButtonLocator, config.TestWaitForElementToAppear);
     await I.waitForText('Equality and diversity questions', config.TestWaitForTextToAppear);

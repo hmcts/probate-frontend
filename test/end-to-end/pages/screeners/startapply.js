04cb77641dfe81a4e13af1640ace987e53af4d1c
Vincent Holt <vleholt@yahoo.co.uk>
2020-11-27 10:20:43 +0000

    reinstate url check

--- test/end-to-end/pages/screeners/startapply.js
+++ test/end-to-end/pages/screeners/startapply.js
@@ -3,6 +3,8 @@
 module.exports = async function() {
     const I = this;
 
+    await I.checkPageUrl('app/steps/ui/screeners/startapply');
+
     const locator = {css: '.govuk-button'};
     await I.waitForElement(locator);
     await I.navByClick(locator);

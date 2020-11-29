a1569f6f8f119695624257c3150bdd89d10e356d
Vincent Holt <vleholt@yahoo.co.uk>
2020-11-29 18:58:48 +0000

    amend crossbrowser tests to us launch darkly config

--- test/end-to-end/pages/documentupload/documentupload.js
+++ test/end-to-end/pages/documentupload/documentupload.js
@@ -1,10 +1,12 @@
 'use strict';
 
+const config = require('config');
+
 module.exports = async function(uploadDocument) {
     const I = this;
 
     await I.checkPageUrl('app/steps/ui/documentupload');
-    await I.waitForVisible({css: '.document-upload__dropzone-text--choose-file'});
+    await I.waitForVisible({css: '.document-upload__dropzone-text--choose-file'}, config.TestWaitForDocumentUpload);
 
     if (uploadDocument) {
         await I.uploadDocumentIfNotMicrosoftEdge();

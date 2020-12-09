00c08549a891e5e4c17d250b845fa4c1f55af952
itoulou <touloumbadjiani@gmail.com>
2020-12-09 16:30:39 +0000

    remove inviteSent checking for a strng

--- app/steps/ui/declaration/index.js
+++ app/steps/ui/declaration/index.js
@@ -132,7 +132,7 @@ class Declaration extends ValidationStep {
             ctx.invitesSent = get(formdata, 'executors.invitesSent');
             ctx.hasMultipleApplicants = ctx.executorsWrapper.hasMultipleApplicants();
             ctx.executorsEmailChanged = ctx.executorsWrapper.hasExecutorsEmailChanged();
-            ctx.hasExecutorsToNotify = ctx.executorsWrapper.hasExecutorsToNotify() && ctx.invitesSent === 'true';
+            ctx.hasExecutorsToNotify = ctx.executorsWrapper.hasExecutorsToNotify() && ctx.invitesSent;
 
             const hasCodicils = (new WillWrapper(formdata.will)).hasCodicils();
             const codicilsNumber = (new WillWrapper(formdata.will)).codicilsNumber();

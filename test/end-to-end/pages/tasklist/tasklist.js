48ca2be449502bf744bf12a35baa6f3934549e70
Megan <meganaliceslattery@gmail.com>
2021-01-07 11:57:37 +0000

    Remove failing check

--- test/end-to-end/pages/tasklist/tasklist.js
+++ test/end-to-end/pages/tasklist/tasklist.js
@@ -1,13 +1,9 @@
 'use strict';
 
-const content = require('app/resources/en/translation/tasklist');
-const testConfig = require('config');
-
 module.exports = async function () {
     const I = this;
 
     await I.checkPageUrl('app/steps/ui/tasklist');
-    await I.waitForText(content.introduction, testConfig.TestWaitForTextToAppear);
     const locator = {css: '.govuk-button'};
     await I.waitForElement(locator);
     await I.click(locator);

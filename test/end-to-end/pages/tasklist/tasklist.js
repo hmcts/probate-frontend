4172919a1f7ed032040b1b3901d0e12d0ae30f35
Elviani Huseyin <elviani.huseyin1@hmcts.net>
2021-03-05 10:19:14 +0000

    PRO-1362: try another way

--- test/end-to-end/pages/tasklist/tasklist.js
+++ test/end-to-end/pages/tasklist/tasklist.js
@@ -8,12 +8,12 @@ const testConfig = require('config');
 module.exports = async function(language ='en') {
     const I = this;
     const taskListContent = language === 'en' ? taskListContentEn : taskListContentCy;
-    if (language === 'en' && language === 'cy') {
+    if (taskListContent && (language === 'en' || language === 'cy')) {
         await I.waitForText(taskListContent.introduction, testConfig.TestWaitForTextToAppear);
     }
     await I.checkPageUrl('app/steps/ui/tasklist');
 
-    for (let i = 0; i <= 5; i++) {
+    for (let i = 0; i <= 2; i++) {
         await I.waitForText(taskListContent.header, testConfig.TestWaitForTextToAppear);
         const result = await I.checkForText(taskListContent.taskStarted, 10);
         if (result === true) {

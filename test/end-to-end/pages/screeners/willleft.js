464119b0689f9b313e545628fea8fe8797a1b00b
isha2040 <58481974+isha2040@users.noreply.github.com>
2021-05-21 13:53:30 +0100

    fix will left js

--- test/end-to-end/pages/screeners/willleft.js
+++ test/end-to-end/pages/screeners/willleft.js
@@ -4,7 +4,7 @@ const contentEn = require('app/resources/en/translation/common');
 const contentCy = require('app/resources/cy/translation/common');
 const willLeftEn = require('app/resources/en/translation/screeners/willleft');
 const willLeftCy = require('app/resources/cy/translation/screeners/willleft');
-const config = require('config');
+//const config = require('config');
 
 module.exports = async function(language ='en', answer) {
     const I = this;
@@ -13,8 +13,9 @@ module.exports = async function(language ='en', answer) {
 
     await I.checkPageUrl('app/steps/ui/screeners/willleft');
     await I.waitForText(willLeftContent.question);
+    await I.seeElement(`#left${answer}`);
     const locator = {css: `#left${answer}`};
-    await I.waitForElement(locator, config.TestWaitForElementToAppear);
+    //await I.waitForElement(locator, config.TestWaitForElementToAppear);
     await I.click(locator);
     await I.navByClick(commonContent.continue);
 };

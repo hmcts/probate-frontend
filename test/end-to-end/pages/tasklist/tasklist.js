ee0b92b2a8d471c0d6168f29a6dcb23f11100a41
Vincent Holt <vleholt@yahoo.co.uk>
2021-05-26 12:02:26 +0100

    amend to waitForClickable

--- test/end-to-end/pages/tasklist/tasklist.js
+++ test/end-to-end/pages/tasklist/tasklist.js
@@ -8,14 +8,9 @@ const testConfig = require('config');
 module.exports = async function(language ='en') {
     const I = this;
     const taskListContent = language === 'en' ? taskListContentEn : taskListContentCy;
-    console.info('about to wait for intro text');
     await I.waitForText(taskListContent.introduction, testConfig.TestWaitForTextToAppear);
-    console.info('about to check url');
     await I.checkPageUrl('app/steps/ui/tasklist');
-    console.info('url checked');
     const locator = {css: '.govuk-button'};
-    await I.waitForElement(locator);
-    console.info('button locator found');
+    await I.waitForClickable(locator);
     await I.click(locator);
-    console.info('button clicked');
 };

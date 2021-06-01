eee8320753ab4adc0dd68acec29b2a897f5b88d3
Vincent Holt <vleholt@yahoo.co.uk>
2021-06-01 14:27:06 +0100

    scroll before enabled/visible check

--- test/end-to-end/pages/tasklist/tasklist.js
+++ test/end-to-end/pages/tasklist/tasklist.js
@@ -9,7 +9,9 @@ module.exports = async function(language ='en') {
     await I.waitForText(taskListContent.introduction, testConfig.TestWaitForTextToAppear);
     await I.checkInUrl('/task-list');
     const locator = {css: '.govuk-button'};
-    await I.waitForEnabled(locator);
+    await I.waitForElement(locator);
     await I.scrollTo(locator);
+    await I.waitForVisible(locator);
+    await I.waitForEnabled(locator);
     await I.click(locator);
 };

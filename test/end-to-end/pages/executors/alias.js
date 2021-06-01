2cead9b6960664fcf15870ed275ac2b431df12dc
Vincent Holt <vleholt@yahoo.co.uk>
2021-06-01 16:03:49 +0100

    try run workers instead of multiple

--- test/end-to-end/pages/executors/alias.js
+++ test/end-to-end/pages/executors/alias.js
@@ -5,6 +5,7 @@ module.exports = async function(language = 'en', answer = null) {
     const commonContent = require(`app/resources/${language}/translation/common`);
     await I.checkInUrl('/executors-alias');
     const locator = {css: `#alias${answer}`};
+    await I.waitForVisible(locator);
     await I.waitForEnabled(locator);
     await I.click(locator);
 

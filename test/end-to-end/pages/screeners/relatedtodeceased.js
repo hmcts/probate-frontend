f022f371e40c708f699fa9b82c64bc790d7f6993
isha2040 <58481974+isha2040@users.noreply.github.com>
2021-06-01 14:55:36 +0100

    Updated code in relatedtodeceased

--- test/end-to-end/pages/screeners/relatedtodeceased.js
+++ test/end-to-end/pages/screeners/relatedtodeceased.js
@@ -2,15 +2,24 @@
 
 const contentEn = require('app/resources/en/translation/common');
 const contentCy = require('app/resources/cy/translation/common');
+const relationshipContentEn = require('app/resources/en/translation/screeners/relatedtodeceased');
+const relationshipContentCy = require('app/resources/cy/translation/screeners/relatedtodeceased');
 
 module.exports = async function(language ='en', answer) {
     const I = this;
     const commonContent = language === 'en' ? contentEn : contentCy;
+    await I.amOnLoadedPage('app/steps/ui/screeners/relatedtodeceased', language);
+    if (language === 'en') {
+        await I.waitForText(relationshipContentEn.question);
+        await I.waitForText(relationshipContentEn.hintText1);
+        await I.waitForText(relationshipContentEn.optionYes);
+    } else {
+        await I.waitForText(relationshipContentCy.question);
+        await I.waitForText(relationshipContentCy.hintText1);
+        await I.waitForText(relationshipContentCy.optionYes);
+    }
 
-    await I.checkPageUrl('app/steps/ui/screeners/relatedtodeceased');
     const locator = {css: `#related${answer}`};
-    await I.waitForElement(locator);
     await I.click(locator);
-
     await I.navByClick(commonContent.continue);
 };

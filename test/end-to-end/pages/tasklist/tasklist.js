7167d6b95da3a2d96d6d65fe7033df81677385c9
Vincent Holt <vleholt@yahoo.co.uk>
2021-06-02 21:29:23 +0100

    give up on running multi-exec in pipeline

--- test/end-to-end/pages/tasklist/tasklist.js
+++ test/end-to-end/pages/tasklist/tasklist.js
@@ -8,14 +8,5 @@ module.exports = async function(language ='en') {
     const taskListContent = require(`app/resources/${language}/translation/tasklist`);
     await I.waitForText(taskListContent.introduction, testConfig.TestWaitForTextToAppear);
     await I.checkInUrl('/task-list');
-    const locator = {css: '.govuk-button'};
-    await I.navByClick(locator);
-
-    /*
-    await I.waitForElement(locator);
-    await I.scrollTo(locator);
-    await I.waitForVisible(locator);
-    await I.waitForEnabled(locator);
-    await I.click(locator);
-    */
+    await I.navByClick({css: '.govuk-button'});
 };

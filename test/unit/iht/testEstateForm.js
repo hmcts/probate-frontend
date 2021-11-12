93bc559feb118cc704bf440f1e122029703cd609
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2021-11-12 15:26:12 +0000

    Add unit test

---
+++ test/unit/iht/testEstateForm.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const initSteps = require('app/core/initSteps');
+const expect = require('chai').expect;
+const steps = initSteps([`${__dirname}/../../../app/steps/action/`, `${__dirname}/../../../app/steps/ui`]);
+const IhtEstateForm = steps.IhtEstateForm;
+
+describe('EstateForm', () => {
+    describe('getUrl()', () => {
+        it('should return the correct url', (done) => {
+            const url = IhtEstateForm.constructor.getUrl();
+            expect(url).to.equal('/estate-form');
+            done();
+        });
+    });
+
+    describe('nextStepOptions()', () => {
+        it('should return the correct options', (done) => {
+            const nextStepOptions = IhtEstateForm.nextStepOptions();
+            expect(nextStepOptions).to.deep.equal({
+                options: [{key: 'estateForm', value: 'optionIHT207', choice: '207'}]
+            });
+            done();
+        });
+    });
+});

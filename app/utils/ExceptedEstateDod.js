f81f57d75f3377afccd3d6d821675572f8f6679a
isha2040 <58481974+isha2040@users.noreply.github.com>
2021-12-07 17:28:16 +0000

    fix yarn lint

--- app/utils/ExceptedEstateDod.js
+++ app/utils/ExceptedEstateDod.js
@@ -7,7 +7,7 @@ class ExceptedEstateDod {
         if (!date) {
             throw new TypeError('no deceased date date of death found');
         }
-        console.log('*******' + config.exceptedEstateDateOfDeath)
+        console.log('*******' + config.exceptedEstateDateOfDeath);
         return new Date(date).getTime() >= new Date(config.exceptedEstateDateOfDeath).getTime();
     }
 }

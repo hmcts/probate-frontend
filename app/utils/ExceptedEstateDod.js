94b5eafc4df9844351e15d1541d70fe80fbcb193
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2021-12-08 15:34:11 +0000

    Add test

--- app/utils/ExceptedEstateDod.js
+++ app/utils/ExceptedEstateDod.js
@@ -7,7 +7,7 @@ class ExceptedEstateDod {
         if (!date) {
             throw new TypeError('no deceased date date of death found');
         }
-        console.log('*******' + config.exceptedEstates.exceptedEstateDateOfDeath);
+        console.log('*******#' + config.exceptedEstates.exceptedEstateDateOfDeath);
         return new Date(date).getTime() >= new Date(config.exceptedEstates.exceptedEstateDateOfDeath).getTime();
     }
 }

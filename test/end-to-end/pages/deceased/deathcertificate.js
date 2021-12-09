4f556cf947ba7a17757ab610c89c4439786fb463
AhsanZX97 <ahsan97@hotmail.co.uk>
2021-12-09 16:26:57 +0000

    started writing e2e pages

--- test/end-to-end/pages/deceased/deathcertificate.js
+++ test/end-to-end/pages/deceased/deathcertificate.js
@@ -4,7 +4,7 @@ module.exports = async function(language ='en', answer = null) {
     const I = this;
     const commonContent = require(`app/resources/${language}/translation/common`);
 
-    await I.checkInUrl('/certificate-interim');
+    await I.checkInUrl('/estate-valued');
     await I.waitForEnabled(`#deathCertificate${answer}`);
     await I.click(`#deathCertificate${answer}`);
     await I.navByClick(commonContent.saveAndContinue, 'button.govuk-button');

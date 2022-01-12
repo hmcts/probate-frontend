e6d50c6a428c1b2cf8e20a488a9bbb11113cf132
ssian2 <88483780+ssian2@users.noreply.github.com>
2022-01-12 09:03:34 +0000

    Update app.js

--- app.js
+++ app.js
@@ -60,8 +60,7 @@ exports.init = function (isA11yTest = false, a11yTestSession = {}, ftValue) {
             .start();
         appInsights.defaultClient.trackTrace({message: 'App insights activated'});
         console.log('App insights activated');
-    }
-    else {
+    } else {
         console.log('App insights not activated');
     }
 

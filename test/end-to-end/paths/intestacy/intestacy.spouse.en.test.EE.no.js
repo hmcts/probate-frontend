a6210bdc9eda2015ad312cf5b0d67b71c53dedec
AhsanZX97 <ahsan97@hotmail.co.uk>
2022-02-15 17:00:30 +0000

    changed local name to noJenkine2e

--- test/end-to-end/paths/intestacy/intestacy.spouse.en.test.EE.no.js
+++ test/end-to-end/paths/intestacy/intestacy.spouse.en.test.EE.no.js
@@ -121,7 +121,7 @@ languages.forEach(language => {
 
         // Thank You
         await I.seeThankYouPage(language);
-    }).tag('@local')
+    }).tag('@noJenkine2e')
         .retry(TestConfigurator.getRetryScenarios());
 
 });

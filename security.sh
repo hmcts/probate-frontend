f001e9dffacb617f57e45a2b00da6039a0cd059e
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2022-03-11 12:31:24 +0000

    DTSPB-2645 update security.sh for Nightly Security scan and enable Nightly Test in Jenkins

--- security.sh
+++ security.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #echo "${SECURITYCONTEXT}" > /zap/security.context
-zap-x.sh -d -host 0.0.0.0 -port 1001 -config api.disablekey=true -config scanner.attackOnStart=true -config view.mode=attack -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true /dev/null 2>&1 &
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+
+zap-x.sh -daemon -host 0.0.0.0 -port 1001 -config database.newsession=3 -config database.newsessionprompt=false -config api.disablekey=true -config scanner.attackOnStart=true -config view.mode=attack -config connection.dnsTtlSuccessfulQueries=-1 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true &
 i=0
 while !(curl -s http://0.0.0.0:1001) > /dev/null
   do
@@ -15,5 +18,6 @@ while !(curl -s http://0.0.0.0:1001) > /dev/null
   zap-cli --zap-url http://0.0.0.0 -p 1001 report -o activescan.html -f html
   echo 'Changing owner from $(id -u):$(id -g) to $(id -u):$(id -u)'
   chown -R $(id -u):$(id -u) activescan.html
+  curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
   cp *.html functional-output/
-  zap-cli -p 1001 alerts -l Medium
+  zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Medium --exit-code False

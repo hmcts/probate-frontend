e84016daf816b0867dbd31329940213c55435d8f
AbdullahShahHMCTS <Abdullah.shah@hmcts.net>
2022-07-27 14:40:43 +0100

    DTSPB-2942 - wait time increase

--- app/assets/javascripts/avaya-webchat-2.js
+++ app/assets/javascripts/avaya-webchat-2.js
@@ -47,7 +47,7 @@
             avayaWebchatClose.hidden = true;
             avayaWebchatMaintenance.hidden = true;
             if(ccState === 'Open'){
-                if(availableAgents > 0 && ewt <= 300){
+                if(availableAgents > 0 && ewt <= 10800){
                     avayaWebchatOpen.hidden = false;
                 }else{
                     avayaAgentBusy.hidden = false;

3379a109aadf7b9623bb65db0d6a5b16563accb3
Marian.Irudayanathan <marian.irudayanathan@justice.gov.uk>
2022-09-09 21:15:31 +0100

    DTSPB-2913 e2e test fix for multiexecutors.js

--- test/end-to-end/helpers/TestConfigurator.js
+++ test/end-to-end/helpers/TestConfigurator.js
@@ -154,7 +154,7 @@ class TestConfigurator {
     }
 
     equalityAndDiversityEnabled() {
-        return this.environment !== 'local';
+        return this.environment !== 'local' || this.environment !== 'preview';
     }
 
     checkFeatureToggle(featureToggleKey) {

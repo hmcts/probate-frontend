deda6af649b216395ac54824fb1ba94c3d74905d
danielbe11 <daniel.bell@solirius.com>
2023-01-20 11:55:08 +0000

    Add setupSecret tests and refactor  setupSecrets

--- app/setupSecrets.js
+++ app/setupSecrets.js
@@ -3,50 +3,52 @@ const {get, set} = require('lodash');
 const {execSync} = require('child_process');
 const logger = require('app/components/logger')('Init');
 
-const setSecret = (secretPath, configPath) => {
-    if (config.has(secretPath)) {
-        set(config, configPath, get(config, secretPath));
-    } else {
-        logger.warn('Cannot find secret with path: ' + secretPath);
+class SetupSecrets {
+    setupSecrets() {
+        if (config.has('secrets.probate')) {
+            this.setSecret('secrets.probate.frontend-redis-access-key', 'redis.password');
+            this.setSecret('secrets.probate.idam-s2s-secret', 'services.idam.service_key');
+            this.setSecret('secrets.probate.ccidam-idam-api-secrets-probate', 'services.idam.probate_oauth2_secret');
+            this.setSecret('secrets.probate.postcode-service-url', 'services.postcode.serviceUrl');
+            this.setSecret('secrets.probate.postcode-service-token2', 'services.postcode.token');
+            this.setSecret('secrets.probate.probate-survey', 'links.survey');
+            this.setSecret('secrets.probate.probate-survey-end', 'links.surveyEndOfApplication');
+            this.setSecret('secrets.probate.probate-service-id', 'payment.serviceId');
+            this.setSecret('secrets.probate.probate-site-id', 'payment.siteId');
+            this.setSecret('secrets.probate.payCaseWorkerUser', 'services.idam.probate_user_email');
+            this.setSecret('secrets.probate.payCaseWorkerPass', 'services.idam.probate_user_password');
+            this.setSecret('secrets.probate.AppInsightsInstrumentationKey', 'appInsights.instrumentationKey');
+            this.setSecret('secrets.probate.launchdarkly-key', 'featureToggles.launchDarklyKey');
+            this.setSecret('secrets.probate.launchdarklyUserkeyFrontend', 'featureToggles.launchDarklyUser.key');
+            this.setSecret('secrets.probate.pcq-token-key', 'services.equalityAndDiversity.tokenKey');
+            this.setSecret('secrets.probate.webchat-avaya-url', 'webchat.avayaUrl');
+            this.setSecret('secrets.probate.webchat-avaya-client-url', 'webchat.avayaClientUrl');
+            this.setSecret('secrets.probate.webchat-avaya-service', 'webchat.avayaService');
+        }
+
+        if (process.env.NODE_ENV === 'dev-aat') {
+            this.setLocalSecret('idam-s2s-secret', 'services.idam.service_key');
+            this.setLocalSecret('ccidam-idam-api-secrets-probate', 'services.idam.probate_oauth2_secret');
+            this.setLocalSecret('launchdarkly-key', 'featureToggles.launchDarklyKey');
+            this.setLocalSecret('launchdarklyUserkeyFrontend', 'featureToggles.launchDarklyUser.key');
+            this.setLocalSecret('payCaseWorkerUser', 'services.idam.probate_user_email');
+            this.setLocalSecret('payCaseWorkerPass', 'services.idam.probate_user_password');
+            this.setLocalSecret('postcode-service-token2', 'services.postcode.token');
+        }
     }
-};
 
-const setupSecrets = () => {
-    if (config.has('secrets.probate')) {
-        setSecret('secrets.probate.frontend-redis-access-key', 'redis.password');
-        setSecret('secrets.probate.idam-s2s-secret', 'services.idam.service_key');
-        setSecret('secrets.probate.ccidam-idam-api-secrets-probate', 'services.idam.probate_oauth2_secret');
-        setSecret('secrets.probate.postcode-service-url', 'services.postcode.serviceUrl');
-        setSecret('secrets.probate.postcode-service-token2', 'services.postcode.token');
-        setSecret('secrets.probate.probate-survey', 'links.survey');
-        setSecret('secrets.probate.probate-survey-end', 'links.surveyEndOfApplication');
-        setSecret('secrets.probate.probate-service-id', 'payment.serviceId');
-        setSecret('secrets.probate.probate-site-id', 'payment.siteId');
-        setSecret('secrets.probate.payCaseWorkerUser', 'services.idam.probate_user_email');
-        setSecret('secrets.probate.payCaseWorkerPass', 'services.idam.probate_user_password');
-        setSecret('secrets.probate.AppInsightsInstrumentationKey', 'appInsights.instrumentationKey');
-        setSecret('secrets.probate.launchdarkly-key', 'featureToggles.launchDarklyKey');
-        setSecret('secrets.probate.launchdarklyUserkeyFrontend', 'featureToggles.launchDarklyUser.key');
-        setSecret('secrets.probate.pcq-token-key', 'services.equalityAndDiversity.tokenKey');
-        setSecret('secrets.probate.webchat-avaya-url', 'webchat.avayaUrl');
-        setSecret('secrets.probate.webchat-avaya-client-url', 'webchat.avayaClientUrl');
-        setSecret('secrets.probate.webchat-avaya-service', 'webchat.avayaService');
+    setSecret(secretPath, configPath) {
+        if (config.has(secretPath)) {
+            set(config, configPath, get(config, secretPath));
+        } else {
+            logger.warn('Cannot find secret with path: ' + secretPath);
+        }
     }
 
-    if (process.env.NODE_ENV === 'dev-aat') {
-        setLocalSecret('idam-s2s-secret', 'services.idam.service_key');
-        setLocalSecret('ccidam-idam-api-secrets-probate', 'services.idam.probate_oauth2_secret');
-        setLocalSecret('launchdarkly-key', 'featureToggles.launchDarklyKey');
-        setLocalSecret('launchdarklyUserkeyFrontend', 'featureToggles.launchDarklyUser.key');
-        setLocalSecret('payCaseWorkerUser', 'services.idam.probate_user_email');
-        setLocalSecret('payCaseWorkerPass', 'services.idam.probate_user_password');
-        setLocalSecret('postcode-service-token2', 'services.postcode.token');
+    setLocalSecret(secretName, configPath) {
+        const result = execSync('az keyvault secret show --vault-name probate-aat -o tsv --query value --name ' + secretName);
+        set(config, configPath, result.toString().replace('\n', ''));
     }
-};
+}
 
-const setLocalSecret = (secretName, configPath) => {
-    const result = execSync('az keyvault secret show --vault-name probate-aat -o tsv --query value --name ' + secretName);
-    set(config, configPath, result.toString().replace('\n', ''));
-};
-
-module.exports = setupSecrets;
+module.exports = SetupSecrets;
--- test/unit/testSetupSecrets.js
+++ test/unit/testSetupSecrets.js
@@ -1,16 +1,37 @@
 const expect = require('chai').expect;
 const {cloneDeep} = require('lodash');
 const config = require('config');
-const proxyquire = require('proxyquire');
+const sinon = require('sinon');
+const rewire = require('rewire');
+const SetupSecrets = rewire('../../app/setupSecrets');
 
 const modulePath = 'app/setupSecrets';
 
 let mockConfig = {};
 
 describe(modulePath, () => {
+    const originalEnv = process.env.NODE_ENV;
     describe('#setup', () => {
         beforeEach(() => {
+            process.env.NODE_ENV = originalEnv;
             mockConfig = cloneDeep(config);
+        });
+
+        afterEach(() => {
+            sinon.restore();
+        });
+
+        it('should set local config values when environment is dev-aat', () => {
+            process.env.NODE_ENV = 'dev-aat';
+            const setLocalSecretStub = sinon.stub(SetupSecrets.prototype, 'setLocalSecret');
+            new SetupSecrets().setupSecrets();
+            sinon.assert.callCount(setLocalSecretStub, 7);
+        });
+
+        it('should not set local config values when environment is not dev-aat', () => {
+            const setLocalSecretStub = sinon.stub(SetupSecrets.prototype, 'setLocalSecret');
+            new SetupSecrets().setupSecrets();
+            sinon.assert.callCount(setLocalSecretStub, 0);
         });
 
         it('should set config values when secrets path is set', () => {
@@ -22,14 +43,14 @@ describe(modulePath, () => {
             };
 
             // Update config with secret setup
-            const setupSecrets = proxyquire(modulePath,
-                {config: mockConfig});
-            setupSecrets();
+            const restore = SetupSecrets.__set__('config', mockConfig);
+            new SetupSecrets().setupSecrets();
 
             expect(mockConfig.redis.password)
                 .to.equal(mockConfig.secrets.probate['frontend-redis-access-key']);
             expect(mockConfig.services.idam.service_key)
                 .to.equal(mockConfig.secrets.probate['idam-s2s-secret']);
+            restore();
         });
 
         it('should not set config values when secrets path is not set', () => {
@@ -38,12 +59,12 @@ describe(modulePath, () => {
                 probate: {
                 }
             };
-            const setupSecrets = proxyquire(modulePath,
-                {config: mockConfig});
-            setupSecrets();
+            const restore = SetupSecrets.__set__('config', mockConfig);
+            new SetupSecrets().setupSecrets();
 
             expect(mockConfig.redis.secret)
                 .to.equal('OVERWRITE_THIS');
+            restore();
         });
 
         it('should only set one config value when single secret path is set', () => {
@@ -54,14 +75,14 @@ describe(modulePath, () => {
             };
 
             // Update config with secret setup
-            const setupSecrets = proxyquire(modulePath,
-                {config: mockConfig});
-            setupSecrets();
+            const restore = SetupSecrets.__set__('config', mockConfig);
+            new SetupSecrets().setupSecrets();
 
             expect(mockConfig.redis.secret)
                 .to.equal('OVERWRITE_THIS');
             expect(mockConfig.services.idam.service_key)
                 .to.equal(mockConfig.secrets.probate['idam-s2s-secret']);
+            restore();
         });
     });
 });

f1e8e1b95881089727d0044136a47373fe4ee934
danielbe11 <daniel.bell@solirius.com>
2023-02-24 17:31:58 +0000

    More

--- test/unit/components/testUtils.js
+++ test/unit/components/testUtils.js
@@ -1,7 +1,9 @@
-const {basicAuthUtil} = require('../../../app/components/utils');
+const {basicAuthUtil, getStore, forceHttps} = require('../../../app/components/utils');
 const {commonNext, commonReq, commonRes} = require('../../util/commonConsts');
 const sinon = require('sinon');
 const proxyquire = require('proxyquire');
+const session = require('express-session');
+const expect = require('chai').expect;
 
 describe('utils', () => {
     let req;
@@ -19,12 +21,14 @@ describe('utils', () => {
         res.send.reset();
         res.set.reset();
         res.sendStatus.reset();
+        res.redirect.reset();
+        next.reset();
     });
 
     describe('basicAuthUtil', () => {
         const TEST_NO_USERNAME_OR_PASSWORD_ERR_MESSAGE = '<h1>Error:</h1><p>Username or password not set.';
 
-        it('no username sends error', (done) => {
+        it('no username sends error', () => {
             const username = '';
             const password = 'passw0rD123';
             const basicAuthUtilMethod = basicAuthUtil(username, password);
@@ -32,10 +36,9 @@ describe('utils', () => {
 
             sinon.assert.calledOnce(res.send);
             sinon.assert.calledWith(res.send, TEST_NO_USERNAME_OR_PASSWORD_ERR_MESSAGE);
-            done();
         });
 
-        it('no password sends error', (done) => {
+        it('no password sends error', () => {
             const username = 'testUsername';
             const password = '';
             const basicAuthUtilMethod = basicAuthUtil(username, password);
@@ -43,10 +46,9 @@ describe('utils', () => {
 
             sinon.assert.calledOnce(res.send);
             sinon.assert.calledWith(res.send, TEST_NO_USERNAME_OR_PASSWORD_ERR_MESSAGE);
-            done();
         });
 
-        it('no user returned by basicAuth', (done) => {
+        it('no user returned by basicAuth', () => {
             const username = 'testUsername';
             const password = 'password';
             authStub = sinon.stub().returns({});
@@ -60,10 +62,9 @@ describe('utils', () => {
             sinon.assert.calledWith(res.sendStatus, 401);
 
             authStub.reset();
-            done();
         });
 
-        it('user with different password', (done) => {
+        it('user with different password', () => {
             const username = 'testUsername';
             const password = 'password';
             authStub = sinon.stub().returns({name: 'testUsername', pass: 'differentPassword'});
@@ -77,10 +78,9 @@ describe('utils', () => {
             sinon.assert.calledWith(res.sendStatus, 401);
 
             authStub.reset();
-            done();
         });
 
-        it('user with different username', (done) => {
+        it('user with different username', () => {
             const username = 'testUsername';
             const password = 'password';
             authStub = sinon.stub().returns({name: 'differentTestUsername', pass: 'password'});
@@ -94,10 +94,9 @@ describe('utils', () => {
             sinon.assert.calledWith(res.sendStatus, 401);
 
             authStub.reset();
-            done();
         });
 
-        it('user with matching user details', (done) => {
+        it('user with matching user details', () => {
             const username = 'testUsername';
             const password = 'password';
             authStub = sinon.stub().returns({name: 'testUsername', pass: 'password'});
@@ -110,7 +109,31 @@ describe('utils', () => {
             sinon.assert.calledOnce(next);
 
             authStub.reset();
-            done();
+        });
+    });
+
+    describe('forceHttps', () => {
+        it('x-forwarded-proto is not https', () => {
+            req.get = sinon.stub().returns('host.');
+            req.url = 'testUrl.com';
+            req.headers['x-forwarded-proto'] = 'http';
+            forceHttps(req, res, next);
+            sinon.assert.calledOnce(res.redirect);
+            sinon.assert.calledWith(res.redirect, 302, 'https://host.testUrl.com');
+        });
+
+        it('x-forwarded-proto is https', () => {
+            req.headers['x-forwarded-proto'] = 'https';
+            forceHttps(req, res, next);
+            sinon.assert.notCalled(res.redirect);
+            sinon.assert.calledOnce(next);
+        });
+    });
+
+    describe('getStore', () => {
+        it('redis disabled', () => {
+            const result = getStore({enabled: 'false'}, session, 100000);
+            expect(result.constructor.name).to.equal('MemoryStore');
         });
     });
 });
--- test/util/commonConsts.js
+++ test/util/commonConsts.js
@@ -8,12 +8,14 @@ const commonReq = {
         'Content-Type': 'application/json',
         'Authorization': 'testToken',
     },
+    get: sinon.stub()
 };
 
 const commonRes = {
     send: sinon.stub(),
     set: sinon.stub(),
-    sendStatus: sinon.stub()
+    sendStatus: sinon.stub(),
+    redirect: sinon.stub()
 };
 
 const commonNext = sinon.stub();

0c5cd9e28eb795c03221dc7ed5d5905e5e9ecd1e
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2023-11-08 10:50:28 +0000

    DTSPB-3601 fix 3601 looping issue

--- test/component/deceased/testDeathCertificate.js
+++ test/component/deceased/testDeathCertificate.js
@@ -4,7 +4,7 @@ const TestWrapper = require('test/util/TestWrapper');
 const testCommonContent = require('test/component/common/testCommonContent.js');
 const IhtMethod = require('app/steps/ui/iht/method');
 const IhtEstateValued = require('app/steps/ui/iht/estatevalued');
-const SubmittedToHmrcBeforeThreshold = require('app/steps/ui/iht/submittedtohmrcbeforethreshold');
+const IhtEstateForm = require('app/steps/ui/iht/estateform');
 const IhtPaper = require('app/steps/ui/iht/paper');
 const config = require('config');
 const caseTypes = require('app/utils/CaseTypes');
@@ -13,7 +13,7 @@ describe('death-certificate-interim', () => {
     let testWrapper;
     const expectedNextUrlForIhtMethod = IhtMethod.getUrl();
     const expectedNextUrlForEstateValued = IhtEstateValued.getUrl();
-    const expectedNextUrlForSubmittedToHmrcBeforeThreshold = SubmittedToHmrcBeforeThreshold.getUrl();
+    const expectedNextUrlForIhtEstateForm = IhtEstateForm.getUrl();
     const expectedNextUrlForIhtPaper = IhtPaper.getUrl();
 
     afterEach(() => {
@@ -83,14 +83,14 @@ describe('death-certificate-interim', () => {
             testWrapper.testRedirect(done, data, expectedNextUrlForIhtMethod);
         });
 
-        it(`test it redirects to submitted-to-hmrc-before-threshold for EE FT on: ${expectedNextUrlForSubmittedToHmrcBeforeThreshold}`, (done) => {
+        it(`test it redirects to submitted-to-hmrc-before-threshold for EE FT on: ${expectedNextUrlForIhtEstateForm}`, (done) => {
             testWrapper = new TestWrapper('DeathCertificateInterim', {ft_excepted_estates: true});
 
             const data = {
                 'dod-date': '2021-12-31',
                 deathCertificate: 'optionDeathCertificate'
             };
-            testWrapper.testRedirect(done, data, expectedNextUrlForSubmittedToHmrcBeforeThreshold);
+            testWrapper.testRedirect(done, data, expectedNextUrlForIhtEstateForm);
         });
         it(`test it redirects to estate valued for EE FT on: ${expectedNextUrlForEstateValued}`, (done) => {
             testWrapper = new TestWrapper('DeathCertificateInterim', {ft_excepted_estates: true});
@@ -102,14 +102,14 @@ describe('death-certificate-interim', () => {
             testWrapper.testRedirect(done, data, expectedNextUrlForEstateValued);
         });
 
-        it(`test it redirects to submitted-to-hmrc-before-threshold FT on but dod before EE dod threshold: ${expectedNextUrlForSubmittedToHmrcBeforeThreshold}`, (done) => {
+        it(`test it redirects to submitted-to-hmrc-before-threshold FT on but dod before EE dod threshold: ${expectedNextUrlForIhtEstateForm}`, (done) => {
             testWrapper = new TestWrapper('DeathCertificateInterim', {ft_excepted_estates: true});
 
             const data = {
                 'dod-date': '2021-12-31',
                 deathCertificate: 'optionDeathCertificate'
             };
-            testWrapper.testRedirect(done, data, expectedNextUrlForSubmittedToHmrcBeforeThreshold);
+            testWrapper.testRedirect(done, data, expectedNextUrlForIhtEstateForm);
         });
 
         it(`test it redirects to iht paper FT on but dod before EE dod threshold: ${expectedNextUrlForIhtPaper}`, (done) => {

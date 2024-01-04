2b2031980c01b5e6c45a85747323679c1163c7cf
Daniel Furnivall <danielfurnivall@gmail.com>
2024-01-04 15:06:45 +0000

    Up not ok

--- app/utils/setupHealthCheck.js
+++ app/utils/setupHealthCheck.js
@@ -8,7 +8,7 @@ const setupHealthCheck = app => {
     app.get('/health/liveness', (req, res) => {
         logger.info('Liveness health check route hit');
         res.status(200).json({
-            status: 'OK',
+            status: 'UP',
             message: 'Liveness check passed',
             name: config.health.service_name,
             host: os.hostname(),
@@ -19,7 +19,7 @@ const setupHealthCheck = app => {
     app.get('/health/readiness', (req, res) => {
         logger.info('Readiness health check route hit');
         res.status(200).json({
-            status: 'OK',
+            status: 'UP',
             message: 'Readiness check passed',
             name: config.health.service_name,
             host: os.hostname(),

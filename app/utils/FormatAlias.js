96e32c8c6d579259fbb84f49f1a44a0bde8a6a21
IswaryaPepakayala <Iswarya.Pepakayala@justice.gov.uk>
2024-09-18 13:08:37 +0100

    DTSPB-4296 Modified alias reason in Legal statement and added few tests

--- app/utils/FormatAlias.js
+++ app/utils/FormatAlias.js
@@ -6,7 +6,8 @@ class FormatAlias {
         const aliasReason = person.aliasReason || person.currentNameReason || '';
         const otherReason = person.otherReason ? person.otherReason : '';
         return (hasMultipleApplicants ? this.formatMultipleApplicantAliasReason(aliasReason, otherReason,
-            isExecutorApplicant, executorCurrentName) : this.formatAliasReason(aliasReason, otherReason)) || '';
+            isExecutorApplicant, executorCurrentName) : this.formatAliasReason(aliasReason, otherReason,
+            isExecutorApplicant)) || '';
     }
 
     static formatMultipleApplicantAliasReason(aliasReason, otherReason, isExecutorApplicant, executorCurrentName) {
@@ -19,25 +20,25 @@ class FormatAlias {
         } else if (aliasReason === 'optionDifferentSpelling') {
             return isExecutorApplicant ? ' ' + executorCurrentName + '‘s name was spelled differently' : ' Their name was spelled differently';
         } else if (aliasReason === 'optionPartOfNameNotIncluded') {
-            return isExecutorApplicant ? ' Part of' + executorCurrentName + ' name was not included ' : ' Part of their name was not included';
+            return isExecutorApplicant ? ' Part of ' + executorCurrentName + '‘s name was not included ' : ' Part of their name was not included';
         } else if (aliasReason === 'optionOther') {
-            return `: ${otherReason}`;
+            return ` ${otherReason}`;
         }
     }
 
-    static formatAliasReason(aliasReason, otherReason) {
+    static formatAliasReason(aliasReason, otherReason, isExecutorApplicant) {
         if (aliasReason === 'optionMarriage') {
-            return 'I got married or formed a civil partnership';
+            return isExecutorApplicant ? ' I got married or formed a civil partnership' : '';
         } else if (aliasReason === 'optionDivorce') {
-            return ' I got divorced or ended my civil partnership';
+            return isExecutorApplicant ? ' I got divorced or ended my civil partnership': '';
         } else if (aliasReason === 'optionDeedPoll') {
-            return ' I changed my name by deed poll';
+            return isExecutorApplicant ? ' I changed my name by deed poll' : '';
         } else if (aliasReason === 'optionDifferentSpelling') {
-            return ' My name was spelled differently';
+            return isExecutorApplicant ? ' My name was spelled differently' : '';
         } else if (aliasReason === 'optionPartOfNameNotIncluded') {
-            return ' Part of my name was not included';
+            return isExecutorApplicant ? ' Part of my name was not included' : '';
         } else if (aliasReason === 'optionOther') {
-            return `: ${otherReason}`;
+            return isExecutorApplicant ? `: ${otherReason}` : '';
         }
     }
 }

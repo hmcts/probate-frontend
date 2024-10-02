9a5986c29f8b2b2929f38312e2864fa978a2ce78
jamiealbertelli <james.albertelli@justice.gov.uk>
2024-10-02 13:09:51 +0100

    DTSPB-4367: Change English and Welsh

--- app/utils/FormatAlias.js
+++ app/utils/FormatAlias.js
@@ -1,22 +1,42 @@
 'use strict';
 
 class FormatAlias {
-    static aliasReason(person, hasMultipleApplicants) {
+    static aliasReason(person, hasMultipleApplicants, isBilingual) {
         person = person || {};
         const aliasReason = person.aliasReason || person.currentNameReason || '';
         const otherReason = person.otherReason ? person.otherReason : '';
-        return this.formatAliasReason(aliasReason, otherReason, hasMultipleApplicants) || '';
+        return this.formatAliasReason(aliasReason, otherReason, hasMultipleApplicants, isBilingual) || '';
     }
 
-    static formatAliasReason(aliasReason, otherReason, hasMultipleApplicants) {
-        if (aliasReason === 'optionMarriage') {
-            return hasMultipleApplicants ? ' got married' : ' I got married';
-        } else if (aliasReason === 'optionDivorce') {
-            return hasMultipleApplicants ? ' got divorced' : ' I got divorced';
-        } else if (aliasReason === 'optionDeedPoll') {
-            return hasMultipleApplicants ? ' changed their name by deed poll' : ' I changed my name by deed poll';
-        } else if (aliasReason === 'optionOther') {
+    // eslint-disable-next-line complexity
+    static formatAliasReason(aliasReason, otherReason, hasMultipleApplicants, isBilingual) {
+        if (aliasReason === 'optionOther') {
             return `: ${otherReason}`;
+        }
+        if (!isBilingual) {
+            if (aliasReason === 'optionMarriage') {
+                return hasMultipleApplicants ? ' they got married or formed a civil partnership' : ' I got married or formed a civil partnership';
+            } else if (aliasReason === 'optionDivorce') {
+                return hasMultipleApplicants ? ' they got divorced or ended their civil partnership' : ' I got divorced or ended a civil partnership';
+            } else if (aliasReason === 'optionDeedPoll') {
+                return hasMultipleApplicants ? ' they changed their name by deed poll' : ' I changed my name by deed poll';
+            } else if (aliasReason === 'optionSpelling') {
+                return hasMultipleApplicants ? ' their name was spelled differently' : ' My name was was spelled differently';
+            } else if (aliasReason === 'optionNameNotIncluded') {
+                return hasMultipleApplicants ? ' part of their name was not included' : ' Part of my name was not included';
+            }
+        } else if (isBilingual) {
+            if (aliasReason === 'optionMarriage') {
+                return hasMultipleApplicants ? ' maent wedi priodi neu wedi ffurfio partneriaeth sifil' : ' Bu imi briodi neu ffurfio partneriaeth sifil';
+            } else if (aliasReason === 'optionDivorce') {
+                return hasMultipleApplicants ? ' maent wedi ysgaru neu wedi dod â’u partneriaeth sifil i ben' : ' Cefais ysgariad neu daeth fy mhartneriaeth sifil i ben';
+            } else if (aliasReason === 'optionDeedPoll') {
+                return hasMultipleApplicants ? ' bu iddynt newid eu henw trwy weithred newid enw' : ' Newidiais fy enw trwy weithred newid enw';
+            } else if (aliasReason === 'optionSpelling') {
+                return hasMultipleApplicants ? ' cafodd eu henw ei sillafu’n wahanol' : ' Cafodd fy enw ei sillafu’n wahanol';
+            } else if (aliasReason === 'optionNameNotIncluded') {
+                return hasMultipleApplicants ? ' ni gafodd rhan o’u henw ei chynnwys' : ' Ni chafodd rhan o fy enw ei gynnwys';
+            }
         }
     }
 }

dbd9cf61d52e07d7ca5c74257304646b162cbcba
Tom Saunders <tom.saunders@justice.gov.uk>
2025-12-03 15:12:31 +0000

    remove feature toggle references.
    also remove links.webchat which doesn't appear to be used anywhere?

--- app/core/steps/Step.js
+++ app/core/steps/Step.js
@@ -70,7 +70,6 @@ class Step {
         }
         ctx = merge(ctx, Sanitize.sanitizeInput(req.body));
         ctx = FeatureToggle.appwideToggles(req, ctx, config.featureToggles.appwideToggles);
-        ctx.isAvayaWebChatEnabled = ctx.featureToggles && ctx.featureToggles.ft_avaya_webchat && ctx.featureToggles.ft_avaya_webchat === 'true';
         ctx.isWebChatEnabled = config.configFeatureToggles.webchatEnabled;
         ctx.isGaEnabled = config.configFeatureToggles.gaEnabled; // this is a boolean type
         return ctx;

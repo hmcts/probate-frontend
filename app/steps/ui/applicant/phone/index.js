669a3081b54d13c7a0204accee2f7c008a4bc33d
Jamie Albertelli <James.Albertelli@justice.gov.uk>
2025-12-19 15:02:05 +0000

    Revert validation changes

--- app/steps/ui/applicant/phone/index.js
+++ app/steps/ui/applicant/phone/index.js
@@ -1,31 +1,11 @@
 'use strict';
 
 const ValidationStep = require('app/core/steps/ValidationStep');
-const PhoneNumberValidator = require('../../../../utils/PhoneNumberValidator');
-const FieldError = require('../../../../components/error');
 
 class ApplicantPhone extends ValidationStep {
 
     static getUrl() {
         return '/applicant-phone';
-    }
-
-    handlePost(ctx, errors, formdata, session) {
-        const applicantPhoneNumber = ApplicantPhone.sanitisePhoneNumber(ctx.phoneNumber);
-
-        const validationResult = PhoneNumberValidator.validateMobilePhoneNumber(applicantPhoneNumber);
-        if (!validationResult.isValid) {
-            errors.push(FieldError('phoneNumber', validationResult.errorType, this.resourcePath, this.generateContent({}, {}, session.language), session.language));
-        }
-
-        return [ctx, errors];
-    }
-
-    static sanitisePhoneNumber(phoneNumber) {
-        phoneNumber = String(phoneNumber).trim();
-        const plusBeforeDigits = (/^\D*\+/).test(phoneNumber);
-        const digitsOnly = phoneNumber.replaceAll(/\D/g, '');
-        return plusBeforeDigits ? '+' + digitsOnly : digitsOnly;
     }
 }
 

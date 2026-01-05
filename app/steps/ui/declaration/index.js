290281e726de27c00a41824acaeb999647aaf814
Jamie Albertelli <James.Albertelli@justice.gov.uk>
2026-01-05 15:52:29 +0000

    DTSPB-4757: Rollback to master after intestacy changes erroneously commited and not reverted

--- app/steps/ui/declaration/index.js
+++ app/steps/ui/declaration/index.js
@@ -290,8 +290,7 @@ class Declaration extends ValidationStep {
         const applicantCurrentName = FormatName.formatName(props.executor, true);
         const aliasSuffix = (typeof props.executor.nameAsOnTheWill !== 'undefined' && props.executor.nameAsOnTheWill === 'optionNo') || props.executor.currentName ? '-alias' : '';
         const aliasReason = FormatAlias.aliasReason(props.executor, props.hasMultipleApplicants, props.language);
-        let content = {};
-        content = {
+        const content = {
             name: props.content[`applicantName${props.multipleApplicantSuffix}${mainApplicantSuffix}${aliasSuffix}${codicilsSuffix}`]
                 .replace('{applicantWillName}', props.executor.isApplicant && (typeof props.executor.nameAsOnTheWill !== 'undefined' && props.executor.nameAsOnTheWill === 'optionNo') ? FormatName.applicantWillName(props.executor) : props.mainApplicantName)
                 .replace(/{applicantCurrentName}/g, applicantCurrentName)

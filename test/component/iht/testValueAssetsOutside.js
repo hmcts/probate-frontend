c19cdf3fe76778e3a2beb0fa397dd31726ba9c87
Tom Saunders <tom.saunders@justice.gov.uk>
2026-02-11 17:10:05 +0000

    make iht tests consistent with others.

--- test/component/iht/testValueAssetsOutside.js
+++ test/component/iht/testValueAssetsOutside.js
@@ -7,7 +7,7 @@ const caseTypes = require('app/utils/CaseTypes');
 
 describe('value-assets-outside-england-wales', () => {
     let testWrapper;
-    const expectedNextUrlForDeceasedAlias = `/intestacy${DeceasedAlias.getUrl()}`;
+    const expectedNextUrlForDeceasedAlias = DeceasedAlias.getUrl();
 
     beforeEach(() => {
         testWrapper = new TestWrapper('ValueAssetsOutside');
@@ -40,7 +40,7 @@ describe('value-assets-outside-england-wales', () => {
             testWrapper.testErrors(done, {}, 'required');
         });
 
-        it(`test it redirects to next page: ${expectedNextUrlForDeceasedAlias}`, (done) => {
+        it(`test it redirects to next page: /intestacy${expectedNextUrlForDeceasedAlias}`, (done) => {
             testWrapper.agent.post('/prepare-session/form')
                 .send({caseType: caseTypes.INTESTACY})
                 .end(() => {
@@ -48,7 +48,7 @@ describe('value-assets-outside-england-wales', () => {
                         netValueAssetsOutsideField: '300000'
                     };
 
-                    testWrapper.testRedirect(done, data, expectedNextUrlForDeceasedAlias);
+                    testWrapper.testRedirect(done, data, `/intestacy${expectedNextUrlForDeceasedAlias}`);
                 });
         });
     });

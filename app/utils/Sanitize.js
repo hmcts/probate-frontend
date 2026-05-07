87f110c2b42e97f4c50f3ecf72591d638ff61e81
Jamie Albertelli <James.Albertelli@justice.gov.uk>
2026-05-07 13:17:04 +0100

    DTSPB-5412: Revert sanitized() func

--- app/utils/Sanitize.js
+++ app/utils/Sanitize.js
@@ -2,22 +2,12 @@
 
 class Sanitize {
     static sanitizeInput(input) {
-        if (input === null || typeof input !== 'object') {
-            return input;
-        }
-
-        if (Array.isArray(input)) {
-            return input.map(item => Sanitize.sanitizeInput(item));
-        }
-
         const sanitized = {};
-
-        for (const key of Object.keys(input)) {
+        for (const key in input) {
             if (!['__proto__', 'constructor', 'prototype'].includes(key)) {
-                sanitized[key] = Sanitize.sanitizeInput(input[key]);
+                sanitized[key] = input[key];
             }
         }
-
         return sanitized;
     }
 }

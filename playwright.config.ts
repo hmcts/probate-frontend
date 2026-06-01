8e51ba26a88c94eac77159ef4b45bc5185588f2b
marian.irudayanathan <marian.irudayanathan@justice.gov.uk>
2026-06-01 14:30:09 +0100

    DTSPB-4687 - Update deceasedDetailsSection.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './test/playwrightTest',
+  timeout: 360000,
 
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -18,6 +19,15 @@ export default defineConfig({
   use: {
     /* Base URL */
     baseURL: process.env.TEST_URL ?? 'http://localhost:3000',
+
+    // Navigation timeout (affects goto, waitForLoadState, etc.)
+    navigationTimeout: 60000,
+
+    // Action timeout (affects click, fill, etc.)
+    actionTimeout: 40000,
+    // headless: false, // Run with visible browser
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
 
     /* Collect trace when retrying */
     trace: 'on-first-retry',

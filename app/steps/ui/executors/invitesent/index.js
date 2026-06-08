c821c01cb6b8dbdb069fcbaf72d938fab7fee61e
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2026-06-08 15:35:25 +0100

    DTSPB-4687 different header for inviteSent

--- app/steps/ui/executors/invitesent/index.js
+++ app/steps/ui/executors/invitesent/index.js
@@ -1,13 +1,19 @@
 'use strict';
 
 const Step = require('app/core/steps/Step');
+const caseTypes = require('../../../../utils/CaseTypes');
 
 class ExecutorsInvitesSent extends Step {
 
     static getUrl () {
         return '/executors-invites-sent';
     }
-
+    getContextData(req) {
+        const ctx = super.getContextData(req);
+        const session = req.session;
+        ctx.caseType = caseTypes.getCaseType(session);
+        return ctx;
+    }
     shouldHaveBackLink() {
         return false;
     }

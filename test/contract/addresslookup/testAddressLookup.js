12f37fa3a8da683744956eade9c88fe7f42edf9a
MarcusHMCTS <Marcus.Ho@justice.gov.uk>
2026-06-17 16:22:40 +0100

    DTSPB-5624 fix contract test for PostcodeLookup.js

--- test/contract/addresslookup/testAddressLookup.js
+++ test/contract/addresslookup/testAddressLookup.js
@@ -3,35 +3,17 @@
 const expect = require('chai').expect;
 const logger = require('app/components/logger')('Init');
 const testConfig = require('config');
-const POSTCODE_SERVICE_TOKEN = testConfig.postcodeLookup.token;
-const OSPlacesClient = require('@hmcts/os-places-client').OSPlacesClient;
-const osPlacesClient = new OSPlacesClient(POSTCODE_SERVICE_TOKEN);
-const osPlacesClientUnauthorised = new OSPlacesClient('INVALID-TOKEN');
+const PostcodeLookup = require('app/services/PostcodeLookup');
+const postcodeLookup = new PostcodeLookup();
 
 describe('Address Lookup API Tests', () => {
-    describe('Basic ping', () => {
-        it('Returns HTTP 403 status', (done) => {
-            osPlacesClientUnauthorised.lookupByPostcode(testConfig.postcodeLookup.singleAddressPostcode)
-                .then(res => {
-                    done(new Error(`Test failed ${JSON.stringify(res)}`));
-                })
-                .catch(err => {
-                    expect(err.name).to.equal('Error');
-                    expect(err.message).to.equal('Authentication failed');
-                    done();
-                });
-        });
-    });
-
     describe('Single address returned for postcode', () => {
         it('Returns single address', (done) => {
-            osPlacesClient.lookupByPostcode(testConfig.postcodeLookup.singleAddressPostcode)
+            postcodeLookup.get(testConfig.postcodeLookup.singleAddressPostcode)
                 .then(res => {
-                    expect(res.addresses.length).to.equal(1);
-                    expect(res.valid).to.equal(true);
-                    expect(res.httpStatus).to.equal(200);
-                    expect(res.addresses[0].organisationName).to.equal(testConfig.postcodeLookup.singleOrganisationName);
-                    expect(res.addresses[0].formattedAddress).to.equal(testConfig.postcodeLookup.singleFormattedAddress);
+                    expect(res.length).to.equal(1);
+                    expect(res[0].postcode).to.equal(testConfig.postcodeLookup.singleAddressPostcode);
+                    expect(res[0].formattedAddress).to.equal(testConfig.postcodeLookup.singleFormattedAddress);
                     done();
                 })
                 .catch(err => {
@@ -43,11 +25,9 @@ describe('Address Lookup API Tests', () => {
 
     describe('Multiple addresses returned for postcode', () => {
         it('Returns multiple addresses', (done) => {
-            osPlacesClient.lookupByPostcode(testConfig.postcodeLookup.multipleAddressPostcode)
+            postcodeLookup.get(testConfig.postcodeLookup.multipleAddressPostcode)
                 .then(res => {
-                    expect(res.addresses.length).to.equal(12);
-                    expect(res.valid).to.equal(true);
-                    expect(res.httpStatus).to.equal(200);
+                    expect(res.length).to.equal(12);
                     done();
                 })
                 .catch(err => {
@@ -59,11 +39,9 @@ describe('Address Lookup API Tests', () => {
 
     describe('Partial postcode test (returns greater number of results)', () => {
         it('No address returned for partial postcode', (done) => {
-            osPlacesClient.lookupByPostcode(testConfig.postcodeLookup.partialAddressPostcode)
+            postcodeLookup.get(testConfig.postcodeLookup.partialAddressPostcode)
                 .then(res => {
-                    expect(res.addresses.length).to.equal(100);
-                    expect(res.valid).to.equal(true);
-                    expect(res.httpStatus).to.equal(200);
+                    expect(res.length).to.equal(100);
                     done();
                 })
                 .catch(err => {
@@ -75,11 +53,9 @@ describe('Address Lookup API Tests', () => {
 
     describe('Invalid postcode test', () => {
         it('No address returned for invalid postcode', (done) => {
-            osPlacesClient.lookupByPostcode(testConfig.postcodeLookup.invalidAddressPostcode)
+            postcodeLookup.get(testConfig.postcodeLookup.invalidAddressPostcode)
                 .then(res => {
-                    expect(res.addresses.length).to.equal(0);
-                    expect(res.valid).to.equal(false);
-                    expect(res.httpStatus).to.equal(200);
+                    expect(res.length).to.equal(0);
                     done();
                 })
                 .catch(err => {
@@ -91,14 +67,27 @@ describe('Address Lookup API Tests', () => {
 
     describe('No postcode entered test', () => {
         it('No address returned for no postcode entered', (done) => {
-            osPlacesClient.lookupByPostcode(testConfig.postcodeLookup.emptyAddressPostcode)
+            postcodeLookup.get(testConfig.postcodeLookup.emptyAddressPostcode)
                 .then(res => {
-                    done(new Error(`Test failed ${JSON.stringify(res)}`));
+                    expect(res.length).to.equal(0);
+                    done();
                 })
                 .catch(err => {
-                    expect(err.name).to.equal('Error');
-                    expect(err.message).to.equal('Missing required postcode');
+                    done(err);
+                });
+        });
+    });
+
+    describe('Basic ping', () => {
+        it('Returns HTTP 401 status', (done) => {
+            testConfig.services.postcode.token = 'invalid-token';
+            postcodeLookup.get(testConfig.postcodeLookup.singleAddressPostcode)
+                .then(res => {
+                    expect(res.length).to.equal(0);
                     done();
+                })
+                .catch(err => {
+                    done(err);
                 });
         });
     });

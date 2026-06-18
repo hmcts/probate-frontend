424da70b0b4bf6e9ad443f96cfb35440dd02c08c
marian.irudayanathan <marian.irudayanathan@justice.gov.uk>
2026-06-18 18:26:05 +0100

    DTSPB-4846 - Remove pa11y tests

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,29 +2,7 @@ import { LintingConfig } from "@hmcts/playwright-common";
 import tseslint from "typescript-eslint";
 
 LintingConfig.ignored.ignores = LintingConfig.ignored.ignores.concat([
-  "*.js",
-  "**/*.js",
-  "node_modules/*",
-  "govuk/*",
-  "public/*",
-  "server/*",
-  "app/*",
-  "coverage",
-  "**/*.min.js",
-  "target/*",
-  "build/*",
-  "functional-output/*",
-  "test/component/*",
-  "test/accessibility/*.js",
-  "**/test/contract/**",
-  "test/unit/**",
-  "test/util/**",
-  "test/smoke/**",
-  "test/*.js",
-  "test/*/*.js",
-  "**/test/end-to-end/*",
-  "mochawesome-report/*/*.js",
-  "output/assets/*.js"
+  "**/*.js"
 ]);
 LintingConfig.tseslintRecommended.files = ["test/playwrightTest/**/*.ts"];
 LintingConfig.tseslintPlugin.files = ["test/playwrightTest/**/*.ts"];

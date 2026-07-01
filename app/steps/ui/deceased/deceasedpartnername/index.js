da0d9a8b0f1b05f9d3eb28e2c123d5abdc0a9920
Tom Saunders <tom.saunders@justice.gov.uk>
2026-07-01 15:59:23 +0100

    scratch.

--- app/steps/ui/deceased/deceasedpartnername/index.js
+++ app/steps/ui/deceased/deceasedpartnername/index.js
@@ -4,6 +4,22 @@ const ValidationStep = require('app/core/steps/ValidationStep');
 
 class DeceasedPartnerName extends ValidationStep {
 
+    constructor(steps, section, templatePath, i18next, schema, language = 'en') {
+        super(steps, section, templatePath, i18next, schema, language);
+    }
+
+    validate(ctx, formdata, language) {
+        const [isValid, errors] = super.validate(ctx, formdata, language);
+        if (!isValid) {
+            errors.forEach(e => {
+                if (e.msg.indexOf('{deceasedName}') !== -1) {
+                    e.msg = e.msg.replace('{deceasedName}', `${ctx.firstName} ${ctx.lastName}`);
+                }
+            });
+        }
+        return [isValid, errors];
+    }
+
     static getUrl() {
         return '/deceased-partner-name';
     }

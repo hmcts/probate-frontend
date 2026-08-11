7b98b12a08d75e4a0b936399d9173d76ce94b408
Jamie Albertelli <James.Albertelli@justice.gov.uk>
2026-08-11 11:51:24 +0100

    DTSPB-5813: Update CSP directives and headers

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ const additionalInvite = require(`${__dirname}/app/routes/additionalInvite`);
 const fs = require('fs');
 const https = require('https');
 const {v4: uuidv4} = require('uuid');
-const nonce = uuidv4().replace(/-/g, '');
 const EligibilityCookie = require('app/utils/EligibilityCookie');
 const eligibilityCookie = new EligibilityCookie();
 const caseTypes = require('app/utils/CaseTypes');
@@ -66,7 +65,6 @@ exports.init = function (isA11yTest = false, a11yTestSession = {}, ftValue) {
         currentYear: new Date().getFullYear(),
         enableTracking: config.enableTracking,
         links: config.links,
-        nonce: nonce,
         documentUpload: {
             validMimeTypes: config.documentUpload.validMimeTypes,
             maxFiles: config.documentUpload.maxFiles,
@@ -97,7 +95,16 @@ exports.init = function (isA11yTest = false, a11yTestSession = {}, ftValue) {
     app.enable('trust proxy');
 
     // Security library helmet to verify 11 smaller middleware functions
-    app.use(helmet());
+    app.use(helmet({
+        xFrameOptions: false,
+        xXssProtection: false
+    }));
+
+    app.use((req, res, next) => {
+        res.locals.cspNonce = uuidv4().replace(/-/g, '');
+        res.locals.globals = {...globals, nonce: res.locals.cspNonce};
+        next();
+    });
 
     app.use(globals.dynatrace.dynatraceUrl, (req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*');
@@ -122,18 +129,8 @@ exports.init = function (isA11yTest = false, a11yTestSession = {}, ftValue) {
             ],
             scriptSrc: [
                 '\'self\'',
-                '\'sha256-GUQ5ad8JK5KmEWmROf3LZd9ge94daqNvd8xy9YS1iDw=\'',
-                '\'sha256-AaA9Rn5LTFZ5vKyp3xOfFcP4YbyOjvWn2up8IKHVAKk=\'',
-                '\'sha256-G29/qSW/JHHANtFhlrZVDZW1HOkCDRc78ggbqwwIJ2g=\'',
-                '\'sha256-BWhcmwio/4/QdqKNw5PKmTItWBjkevCaOUbLkgW5cHs=\'',
-                '\'sha256-L7viC3kUpXu9uCOi97VqCR2bLlMwSQlmLmSuuQ93ngU=\'',
-                '\'sha256-nWZRr0RF4OANYiYcCteeOrMWiiSKIEIE+qPfFTq/WyI=\'',
-                '*.google-analytics.com',
-                'https://*.dynatrace.com',
-                '*.googletagmanager.com',
-                `'nonce-${nonce}'`,
-                'tagmanager.google.com',
-                config.webchat.kerv.genesysBaseUrl,
+                '\'strict-dynamic\'',
+                (req, res) => `'nonce-${res.locals.cspNonce}'`,
             ],
             connectSrc: [
                 '\'self\'',
@@ -196,7 +193,6 @@ exports.init = function (isA11yTest = false, a11yTestSession = {}, ftValue) {
     }));
 
     app.use(nocache());
-    app.use(helmet.xssFilter({setOnOldIE: true}));
 
     app.use(helmet.strictTransportSecurity({
         maxAge: 31536000,
@@ -204,6 +200,7 @@ exports.init = function (isA11yTest = false, a11yTestSession = {}, ftValue) {
 
     app.use((req, res, next) => {
         res.header('X-Robots-Tag', 'noindex');
+        res.header('Permissions-Policy', 'geolocation=(), camera=(), microphone=()');
         next();
     });
 
--- test/component/testHelmet.js
+++ test/component/testHelmet.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const {assert} = require('chai');
+const expect = require('chai').expect;
 const proxyquire = require('proxyquire');
 const sinon = require('sinon');
 const express = require('express');
 const helmet = require('helmet');
+const request = require('supertest');
 
 const app = proxyquire('app', {
     'express': express,
@@ -36,5 +38,79 @@ describe('app-config-helmet', () => {
             `Expected strictTransportSecurity to be called once but was called ${called} times`);
 
         done();
+    });
+
+    it('should set permissions-policy and avoid deprecated client-side headers', (done) => {
+        const server = app.init();
+
+        request(server.app)
+            .get('/health')
+            .end((err, res) => {
+                server.http.close();
+                if (err) {
+                    return done(err);
+                }
+
+                expect(res.headers['permissions-policy']).to.equal('geolocation=(), camera=(), microphone=()');
+                // eslint-disable-next-line no-undefined
+                expect(res.headers['x-frame-options']).to.equal(undefined);
+                // eslint-disable-next-line no-undefined
+                expect(res.headers['x-xss-protection']).to.equal(undefined);
+
+                return done();
+            });
+    });
+
+    it('should use nonce-based script-src policy without unsafe-inline', (done) => {
+        const server = app.init();
+
+        request(server.app)
+            .get('/health')
+            .end((err, res) => {
+                server.http.close();
+                if (err) {
+                    return done(err);
+                }
+
+                const csp = res.headers['content-security-policy'];
+                expect(csp).to.contain('script-src \'self\' \'strict-dynamic\'');
+                expect(csp).to.match(/script-src[^;]*'nonce-[^']+'/);
+                expect(csp).to.not.match(/script-src[^;]*'unsafe-inline'/);
+
+                return done();
+            });
+    });
+
+    it('should rotate nonce for each request', (done) => {
+        const server = app.init();
+
+        request(server.app)
+            .get('/health')
+            .end((firstErr, firstRes) => {
+                if (firstErr) {
+                    server.http.close();
+                    return done(firstErr);
+                }
+
+                request(server.app)
+                    .get('/health')
+                    .end((secondErr, secondRes) => {
+                        server.http.close();
+                        if (secondErr) {
+                            return done(secondErr);
+                        }
+
+                        const firstNonce = (firstRes.headers['content-security-policy'] || '').match(/'nonce-([^']+)'/);
+                        const secondNonce = (secondRes.headers['content-security-policy'] || '').match(/'nonce-([^']+)'/);
+
+                        // eslint-disable-next-line no-unused-expressions
+                        expect(firstNonce && firstNonce[1]).to.exist;
+                        // eslint-disable-next-line no-unused-expressions
+                        expect(secondNonce && secondNonce[1]).to.exist;
+                        expect(firstNonce[1]).to.not.equal(secondNonce[1]);
+
+                        return done();
+                    });
+            });
     });
 });

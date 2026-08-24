c378975197efc2010e813e5546fc74032ffa5047
sadaque-khan-hmcts <Sadaque.Khan@justice.gov.uk>
2026-08-24 02:39:16 +0100

    [DTSPB-5795-5789] revert(test): restore component and unit tests to parent baseline

--- test/unit/applicant/testAnyOtherWholeBloodSiblings.js
+++ test/unit/applicant/testAnyOtherWholeBloodSiblings.js
@@ -177,27 +177,5 @@ describe('AnyOtherWholeSiblings', () => {
             assert.isUndefined(ctx.anyPredeceasedWholeSiblings);
             assert.isUndefined(ctx.allWholeNiecesAndWholeNephewsOver18);
         });
-
-        it('test it cleans up context when answer remains no', () => {
-            const ctx = {
-                anyOtherWholeSiblings: 'optionNo',
-                anyPredeceasedWholeSiblings: 'optionYesAll',
-                anySurvivingWholeNiecesAndWholeNephews: 'optionYes',
-                allWholeNiecesAndWholeNephewsOver18: 'optionYes',
-                allWholeSiblingsOver18: 'optionYes'
-            };
-            const formdata = {
-                applicant: {
-                    anyOtherWholeSiblings: 'optionNo'
-                }
-            };
-
-            AnyOtherWholeSiblings.action(ctx, formdata);
-
-            assert.isUndefined(ctx.allWholeSiblingsOver18);
-            assert.isUndefined(ctx.anyPredeceasedWholeSiblings);
-            assert.isUndefined(ctx.anySurvivingWholeNiecesAndWholeNephews);
-            assert.isUndefined(ctx.allWholeNiecesAndWholeNephewsOver18);
-        });
     });
 });
